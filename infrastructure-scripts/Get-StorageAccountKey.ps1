7bb3e3a48c509204912767d08edddc925045d372
Craig Gumbley <craiggumbley@gmail.com>
2019-02-20 10:54:32 +0000

    add automation scripts

---
+++ infrastructure-scripts/Get-StorageAccountKey.ps1
@@ -0,0 +1,61 @@
+<#
+
+.SYNOPSIS
+Return either the Primary or Seconday connection String to the Consig Storage account and Write to a VSTS variable.
+
+.DESCRIPTION
+Return either the Primary or Seconday connection String to the Consig Storage account and Write to a VSTS variable.
+
+.PARAMETER Name
+The name of the Storage Account
+
+.PARAMETER useSecondary
+Boolean Switch to Return Secondary String
+
+.EXAMPLE
+.\Get-GetStorageAccountKey.ps1  -Name stracc
+
+.EXAMPLE
+.\Get-GetStorageAccountKey.ps1 -Name stracc -useSecondary
+
+.EXAMPLE
+.\Get-GetStorageAccountKey.ps1  -Name stracc -OutputVariable "CustomOutputVariable"
+
+#>
+
+Param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Name,
+    [Parameter(Mandatory = $false)]
+    [switch]$UseSecondary),
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String]$OutputVariable = "StorageConnectionString"
+
+try {
+
+    # --- Check if storage account exists in our subscription
+    Write-Host "Checking for existing Storage Account"
+    $StorageAccount = Get-AzureRmResource -ResourceName $Name -ErrorAction SilentlyContinue
+
+    # --- If the Storage Account doesn't exist, erorr
+    if (!$StorageAccount) {
+       throw "StorageAccount $Name Does not exist"
+    }
+
+    # --- Get the key and set the env variable
+    $Key = Invoke-AzureRmResourceAction -Action listKeys -ResourceType "Microsoft.ClassicStorage/storageAccounts" -ApiVersion "2016-11-01" -ResourceGroupName $($StorageAccount.ResourceGroupName) -ResourceName $($StorageAccount.Name) -force
+
+    if ($UseSecondary.IsPresent) {
+        $ConnectionString = "DefaultEndpointsProtocol=https;AccountName=$($Name);AccountKey=$($Key.SecondaryKey)"
+    }
+    else {
+        $ConnectionString = "DefaultEndpointsProtocol=https;AccountName=$($Name);AccountKey=$($Key)"
+    }
+
+    Write-Output ("##vso[task.setvariable variable=$($OutputVariable);issecret=true]$($ConnectionString)")
+}
+catch {
+    throw "$_"
+}
---
+++ infrastructure-scripts/New-StorageAccountContainer.ps1
@@ -0,0 +1,88 @@
+<#
+.SYNOPSIS
+Create an ARM Storage Container.
+
+.DESCRIPTION
+Create one or more containers in a storage account
+
+.PARAMETER Location
+The location of the Resource Group. This is limited to West Europe and North Europe
+
+.PARAMETER Name
+The name of the Storage Account
+
+.PARAMETER ContainerName
+The names of one or more Containers to create in the Storage Account
+
+.PARAMETER ContainerPermission
+The permission on the Container(s).  The acceptable values are 'Container', 'Blob' or 'Off'
+
+.EXAMPLE
+.\New-StorageAccountContainer.ps1 -Location "West Europe" -Name stracc -ContainerName public -ContainerPermission "Blob"
+
+.EXAMPLE
+.\New-StorageAccountContainer.ps1 -Location "West Europe" -Name stracc -ContainerName public,private,images -ContainerPermission "Blob"
+
+#>
+
+Param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("West Europe", "North Europe")]
+    [String]$Location = $ENV:Location,
+    [Parameter(Mandatory = $true)]
+    [String]$Name,
+    [Parameter(Mandatory = $true)]
+    [String[]]$ContainerName,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Container", "Blob", "Off")]
+    [String]$ContainerPermission = "Off"
+
+)
+
+try {
+    # --- Import Azure Helpers
+    Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Azure.psm1).Path
+    Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
+
+    Write-Host "Resolving storage account"
+    # --- Check if storage account exists in our subscription
+    if ((((Get-Module AzureRM -ListAvailable | Sort-Object { $_.Version.Major } -Descending).Version.Major))[0] -gt 5) {
+        $StorageAccountResource = Get-AzureRmResource -Name $Name -ResourceType "Microsoft.Storage/storageAccounts"
+    }
+    else {
+        $StorageAccountResource = Find-AzureRmResource -ResourceNameEquals $Name -ResourceType "Microsoft.Storage/storageAccounts"
+    }
+    if (!$StorageAccountResource) {
+        throw "Could not find storage account $Name"
+    }
+
+    $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResource.ResourceGroupName -Name $Name
+    $StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $StorageAccountResource.ResourceGroupName -Name $Name)[0].Value
+    $StorageAccountContext = New-AzureStorageContext -StorageAccountName $Name -StorageAccountKey $StorageAccountKey
+
+    # --- Create containers in the storage account if required
+    if ($ContainerName -and $StorageAccount) {
+        foreach ($Container in $ContainerName) {
+            Write-Host "Checking for existing container"
+            $ContainerExists = Get-AzureStorageContainer -Context $StorageAccountContext -Name $Container -ErrorAction SilentlyContinue
+            if (!$ContainerExists) {
+                try {
+                    Write-Host "Creating container $Container"
+                    $null = New-AzureStorageContainer -Context $StorageAccountContext -Name $Container -Permission $ContainerPermission
+                }
+                catch {
+                    throw "Could not create container $Container : $_"
+                }
+            }
+        }
+    }
+
+    # --- If the storage account exists in this subscription get the key and set the env variable
+    if ($StorageAccount) {
+        $ConnectionString = "DefaultEndpointsProtocol=https;AccountName=$($Name);AccountKey=$($StorageAccountKey)"
+        Write-Output ("##vso[task.setvariable variable=StorageConnectionString; issecret=true;]$($ConnectionString)")
+    }
+}
+catch {
+    throw $_
+}
