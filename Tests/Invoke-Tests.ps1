c66241280b245f833d676588cc2ed04cd3f44617
Dale Morson <dale.morson@gmail.com>
2019-04-23 14:03:24 +0100

    Install Pester and PSScriptAnalyzer as part of the Invoke-Tests.ps1 script

--- Tests/Invoke-Tests.ps1
+++ Tests/Invoke-Tests.ps1
@@ -33,6 +33,15 @@ if ($TestType -ne 'All') {
     $TestParameters['Tag'] = $TestType
 }
 
+# Install required modules
+$RequiredModules = @("Pester", "PSScriptAnalyzer")
+foreach ($Module in $RequiredModules) {
+    if (!(Get-Module -Name $Module -ListAvailable)) {
+        Write-Verbose "Installing $Module"
+        Install-Module -Name $Module -Scope CurrentUser -Force
+    }
+}
+
 # Remove previous runs
 Remove-Item "$PSScriptRoot\TEST-*.xml"
 
