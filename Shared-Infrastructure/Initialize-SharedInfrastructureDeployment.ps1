a49fb866970dd3427cd3c1d49464a5692cac5a8d
Craig Gumbley <craiggumbley@gmail.com>
2019-06-12 13:12:52 +0100

    Add help to deployment script

--- Shared-Infrastructure/Initialize-SharedInfrastructureDeployment.ps1
+++ Shared-Infrastructure/Initialize-SharedInfrastructureDeployment.ps1
@@ -1,3 +1,39 @@
+<#
+.SYNOPSIS
+Shared infrastructure deployment helper
+
+.DESCRIPTION
+Pre deployment script for shared infrastructure deployments. It is deisnged to be ran in the context of a
+build agent or from a local machine.
+
+Build agent deployments:
+    * This script only prepares the deployment environment
+    * Ensure that the correct template environment variables are present in the release definition
+    * Use the ARM template deployment task
+    * Secret variables must be passed as override parameters to the ARM tepmlate deployment release task
+Local deployments:
+    * ONLY to be used for testing and NOT against any environments that are in service
+    * The script looks for matching environment variables. If they are not present you will be prompted to enter the values
+    * When not in the context of a build server, this script will create the deployment.
+
+.PARAMETER SubscriptionAbbreviation
+Abbreviation of the subscription. It is possible for a subscription to have many environments, for example,
+a development subscription can contain many development environments.
+
+Default: DEV
+
+.PARAMETER EnvironmentNames
+A JSON array of environments to create in a subscription. For example:
+"["AT","TEST""]
+
+Default: $ENV:EnvironmentNames
+
+.PARAMETER Location
+The location of the resources.
+
+Default: West Europe
+
+#>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $false)]
