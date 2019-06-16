5c60c528bb537963664887f0e81be1e682a1402c
Craig Gumbley <craiggumbley@gmail.com>
2019-06-16 21:17:20 +0000

    Give test runner a better verb

--- AzureDevOps-Tasks/EnvironmentConfiguration/Tests/Invoke-TestRunner.ps1
+++
@@ -1,9 +0,0 @@
-# --- Agent properties
-$ENV:System_Culture = "EN_GB"
-
-# --- Schema properties
-$ENV:EventsApiBaseUrl = "https://events.test.com"
-$ENV:EventsApiClientToken = "xxxxxxxxxxlksmdflkm3lkmlkm"
-$ENV:PaymentsEnabled = $true
-$ENV:PaymentsInt = 1
-$ENV:PaymentsNumber = 1.0
