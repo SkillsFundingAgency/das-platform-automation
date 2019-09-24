40b54134669b221217c722ec2cb111c35887e2e1
Dale Morson <dale.morson@outlook.com>
2019-09-24 15:22:06 +0100

    Minor structure changes.

--- Infrastructure-Scripts/Set-AzResourceGroupTags.ps1
+++ Infrastructure-Scripts/Set-AzResourceGroupTags.ps1
@@ -52,65 +52,52 @@ try {
     $ResourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
 
     if (!$ResourceGroup) {
-
         Write-Verbose -Message "Resource group $ResourceGroupName doesn't exist, creating resource group"
         New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Tag $Tags
-
     }
 
     else {
-
         Write-Verbose -Message "Resource group $ResourceGroupName exists, validating tags"
         $UpdateTags = $false
 
         if ($ResourceGroup.Tags) {
-
             # --- Check existing tags and update if necessary
             $UpdatedTags = $ResourceGroup.Tags
             foreach ($Key in $Tags.Keys) {
-
                 Write-Verbose "Current value of Resource Group Tag $Key is $($ResourceGroup.Tags[$Key])"
 
                 if ($($ResourceGroup.Tags[$Key]) -eq $($Tags[$Key])) {
-
                     Write-Verbose -Message "Current value of tag ($($ResourceGroup.Tags[$Key])) matches parameter ($($Tags[$Key]))"
-
                 }
 
                 elseif ($null -eq $($ResourceGroup.Tags[$Key])) {
-
                     Write-Verbose -Message ("Tag value is not set, adding tag {0} with value {1}" -f $Key, $Tags[$Key])
                     $UpdatedTags[$Key] = $Tags[$Key]
                     $UpdateTags = $true
-
                 }
 
                 else {
-
                     Write-Verbose -Message ("Tag value is incorrect, setting tag {0} with value {1}" -f $Key, $Tags[$Key])
                     $UpdatedTags[$Key] = $Tags[$Key]
                     $UpdateTags = $true
-
                 }
+
             }
 
         }
 
         else {
-
             # --- No tags to check, just update with the passed in tags
             $UpdatedTags = $Tags
             $UpdateTags = $true
-
         }
 
         if ($UpdateTags) {
-
             Write-Verbose -Message "Replacing existing tags:"
             $UpdatedTags
             Set-AzResourceGroup -Name $ResourceGroup.ResourceGroupName -Tag $UpdatedTags
-
         }
+
     }
 
 }
