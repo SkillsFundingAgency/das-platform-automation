69a747884dc38e9a5ac83b9609f26b3ce2caf0bd
Mike Wevill <mwevill@gmail.com>
2019-10-25 11:48:27 +0100

    Added Invoke-CdnContentPurge and tests

---
+++ Infrastructure-Scripts/Invoke-CdnContentPurge.ps1
@@ -0,0 +1,60 @@
+
+<#
+.SYNOPSIS
+Purges the content from an Azure Content Delivery Network (CDN)
+
+.DESCRIPTION
+Purges the content from an Azure Content Delivery Network (CDN)
+
+.PARAMETER CDNProfileResourceGroup
+The Resource Group of the CDN
+
+.PARAMETER CDNProfileName
+The CDN Profile Name
+
+.PARAMETER CDNEndPointName
+The CDN EndPoint Name
+
+.PARAMETER PurgeContent
+The assest you wish to purge from the edge nodes
+Single URL Purge: Purge individual asset by specifying the full URL, e.g., "/pictures/image1.png" or "/pictures/image1"
+Wildcard purge: Purge all folders, sub-folders, and files under an endpoint with "/*"  e.g. "/* " or "/pictures/*"
+Root domain purge: Purge the root of the endpoint with "/" in the path
+
+.EXAMPLE
+
+Invoke-CdnContentPurge.ps1 -CDNProfileResourceGroup aResourceGroup -CDNProfileName aCdnProfile -CDNEndPointName aCdnEndpoint -PurgeContent "/*"
+
+.NOTES
+
+    Suppressed Script Analyzer rules:
+         - PSUseShouldProcessForStateChangingFunctions - The function does not alter the state of an object
+
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]$CDNProfileResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [String]$CDNProfileName,
+    [Parameter(Mandatory = $true)]
+    [String]$CDNEndPointName,
+    [Parameter(Mandatory = $false)]
+    [String]$PurgeContent = ""
+)
+try {
+    if ( $PurgeContent -eq "" ) {
+        throw "Purge Content blank will not run purge"
+    }
+        # --- Set CDN EndPoint
+    $CDNEndpoint = Get-AzCdnEndpoint -ResourceGroupName $CDNProfileResourceGroup -ProfileName $CDNProfileName -EndpointName $CDNEndpointName
+    if (!$CDNEndpoint){
+        throw "CDN Endpoint Does not exist"
+    }
+        # ---> Purging CDN EndPoint
+    Unpublish-AzCdnEndpointContent  -ResourceGroupName $CDNProfileResourceGroup -ProfileName $CDNProfileName -EndpointName $CDNEndpointName -PurgeContent $PurgeContent
+}
+catch {
+    throw "$_"
+}
+
