ac28ecc10daf65d05ca010e93cf6bf63e69abd23
Mike Wevill <mwevill@gmail.com>
2019-10-25 13:41:15 +0100

    Formatting to Unit Tests

--- Tests/UT.Invoke-CdnContentPurge.Tests.ps1
+++ Tests/UT.Invoke-CdnContentPurge.Tests.ps1
@@ -5,14 +5,14 @@ Describe "Invoke-CdnContentPurge Unit Tests" -Tags @("Unit") {
 
     Context "Purge Content Parameter Blank" {
         It "Should throw an exception to warn that no content purge will be run" {
-            { ./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.blankPurge  } | Should throw "Purge Content blank will not run purge"
+            { ./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.blankPurge } | Should throw "Purge Content blank will not run purge"
         }
     }
 
     Context "Resource Group or CDN Profile does not exist" {
         It "The specified Resource Group or CDN Profile was not found in the subscription, throw an error" {
             Mock Get-AzCdnEndpoint -MockWith { Return $null }
-            { ./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.purgeContent  } | Should throw "CDN Endpoint Does not exist"
+            { ./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.purgeContent } | Should throw "CDN Endpoint Does not exist"
             Assert-MockCalled -CommandName 'Get-AzCdnEndpoint'  -Times 1 -Scope It
         }
     }
@@ -26,9 +26,9 @@ Describe "Invoke-CdnContentPurge Unit Tests" -Tags @("Unit") {
                     }
                 }
                 return $cdnEndpointExists
-             }
-            Mock Unpublish-AzCdnEndpointContent -MockWith {Return $null}
-            {./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.purgeContent }  | Should Not Throw
+            }
+            Mock Unpublish-AzCdnEndpointContent -MockWith { Return $null }
+            { ./Invoke-CdnContentPurge -CDNProfileResourceGroup $Config.resourceGroupName -CDNProfileName $Config.CdnProfileName -CDNEndPointName $Config.CDNEndPointName -PurgeContent $Config.purgeContent } | Should Not Throw
             Assert-MockCalled -CommandName 'Get-AzCdnEndpoint' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Unpublish-AzCdnEndpointContent' -Times 1 -Scope It
         }
