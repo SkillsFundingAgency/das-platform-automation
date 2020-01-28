eb596e2cef9da21afa3331371cfac10a06f1044a
Jack <jack>
2020-01-28 22:02:42 +0000

    Updated OWASP check unit test: invalid customerId and SharedKey

--- Tests/UT.Import-OwaspDependencyCheckResults.Tests.ps1
+++ Tests/UT.Import-OwaspDependencyCheckResults.Tests.ps1
@@ -2,22 +2,35 @@ $Config = Get-Content $PSScriptRoot\..\Tests\Unit.Tests.Config.json -Raw | Conve
 Set-Location $PSScriptRoot\..\Code-Analysis-Scripts\
 
 Describe "Import-OwaspDependencyCheckResults Unit Tests" -Tags @("Unit") {
-    It "Should return 200 if successful" {
+
+    It "Should fail with invalid CustomerId" {
         $TestParams = @{
             CustomerId = $Config.customerId
-            SharedKey  = $Config.sharedKey
+            SharedKey  = $Config.validBase64String
         }
         $ENV:BUILD_REPOSITORY_NAME = $Config.repositoryName
         $ENV:BUILD_SOURCEBRANCHNAME = $Config.branchName
         $ENV:BUILD_BUILDNUMBER = $Config.buildNumber
         $ENV:BUILD_SOURCEVERSION = $Config.sourceVersion
-        Mock Invoke-WebRequest {
-            [pscustomobject]@{ StatusCode = 200 }
-        }
         Mock Import-Csv {
             [pscustomobject]@{ Project = 'OWASP Dependency Check' }
         }
-        $result = ./Import-OwaspDependencyCheckResults.ps1 @TestParams
-        $result | Should be 200
+        { ./Import-OwaspDependencyCheckResults @TestParams } | Should throw "No such host is known"
+    }
+
+    It "Should fail with invalid Base-64 string for SharedKey" {
+        $TestParams = @{
+            CustomerId = $Config.customerId
+            SharedKey  = $Config.invalidBase64String
+        }
+        $ENV:BUILD_REPOSITORY_NAME = $Config.repositoryName
+        $ENV:BUILD_SOURCEBRANCHNAME = $Config.branchName
+        $ENV:BUILD_BUILDNUMBER = $Config.buildNumber
+        $ENV:BUILD_SOURCEVERSION = $Config.sourceVersion
+        Mock Import-Csv {
+            [pscustomobject]@{ Project = 'OWASP Dependency Check' }
+        }
+        $ErrorMessage = 'Exception calling "FromBase64String" with "1" argument(s): "The input is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters."'
+        { ./Import-OwaspDependencyCheckResults @TestParams } | Should throw $ErrorMessage
     }
 }
