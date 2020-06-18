0b164d9a4516f0b85224a0a5786443827d66b2b3
adam230594 <47275045+adam230594@users.noreply.github.com>
2020-06-18 16:45:01 +0100

    Adding Import-ApimSwaggerApiDefinition +semver: minor (#64)
    * initial creation of Import-ApimSwaggerApiDefinition
    
    * remove delete functionality
    
    * Added ApplicationIdentifierUri parameter
    
    * Set Policy in foreach
    
    * Edited header name to X-Version and corrected ServiceUrl variable to ApiBaseUrl
    
    * Removed settings.json
    
    * Added settings.json to .gitignore
    
    * Assign API to Products
    
    * Assign API to Products
    
    * updated examples
    
    * Corrected .gitignore to be folder names instead of <folder name>/
    
    * added unit tests for import swagger
    
    * added unit tests for import swagger
    
    * fixed script analyzer issues
    
    * fixed script analyzer issues #2
    
    * use powershell core
    
    Co-authored-by: Mohamad Adam <accadam230594@outlook.com>
    Co-authored-by: jack-education <jack>

---
+++ Infrastructure-Scripts/Import-ApimSwaggerApiDefinition.ps1
@@ -0,0 +1,101 @@
+<#
+    .SYNOPSIS
+    Update an APIM API with a swagger definition with multiple versions
+    .DESCRIPTION
+    Update an APIM API with a swagger definition with multiple versions
+    .PARAMETER ApimResourceGroup
+    The name of the resource group that contains the APIM instnace
+    .PARAMETER InstanceName
+    The name of the APIM instance
+    .PARAMETER ApiName
+    The name of the API to update
+    .PARAMETER ApiPath
+    The URL suffix that APIM will apply to the API URL.
+    .PARAMETER ApiBaseUrl
+    The full path to the swagger defintion
+    .PARAMETER ApplicationIdentifierUri
+    The Application Identifier URI of the API app registration
+    .PARAMETER ProductId
+    The Id of the Product that the API will be assigned to
+    .EXAMPLE
+    Import-ApimSwaggerApiDefinition -ApimResourceGroup das-at-foobar-rg -InstanceName das-at-foobar-apim -ApiName foobar-api -ApiBaseUrl "https://at-foobar-api.apprenticeships.education.gov.uk" -ApiPath "foo-bar" -ApplicationIdentifierUri "https://citizenazuresfabisgov.onmicrosoft.com/das-at-foobar-as-ar" -ProductId ProductId
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]$ApimResourceGroup,
+    [Parameter(Mandatory = $true)]
+    [String]$InstanceName,
+    [Parameter(Mandatory = $true)]
+    [String]$ApiName,
+    [Parameter(Mandatory = $true)]
+    [String]$ApiPath,
+    [Parameter(Mandatory = $true)]
+    [String]$ApiBaseUrl,
+    [Parameter(Mandatory = $true)]
+    [String]$ApplicationIdentifierUri,
+    [Parameter(Mandatory = $true)]
+    [String]$ProductId
+)
+
+
+function Read-SwaggerHtml ($ApiBaseUrl) {
+    try {
+        Invoke-WebRequest "$ApiBaseUrl/index.html"
+    }
+    catch {
+        throw "Could not find swagger page at: $ApiBaseUrl/index.html"
+    }
+
+}
+
+function Get-AllSwaggerFilePaths ($SwaggerHtml) {
+    $Paths = @()
+    $MatchedStrings = Select-String '/swagger/v\d/swagger.json' -input $SwaggerHtml -AllMatches
+    foreach ($MatchedString in $MatchedStrings){
+        $Paths = $MatchedString.matches -split ' '
+    }
+    $Paths
+}
+
+$PolicyString = "<policies><inbound><base/><authentication-managed-identity resource=`"$ApplicationIdentifierUri`"/></inbound><backend><base/></backend><outbound><base/></outbound><on-error><base/></on-error></policies>"
+
+# Check if the APIM Instance exists
+$ApimInstanceExists = Get-AzApiManagement -ResourceGroupName $ApimResourceGroup -Name $InstanceName
+if (!$ApimInstanceExists) {
+    throw "APIM Instance: $InstanceName does not exist in resource group: $ApimResourceGroup"
+}
+
+Write-Verbose "Building APIM context for $ApimResourceGroup\$InstanceName"
+$Context = New-AzApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $InstanceName
+
+# Get all version paths
+$SwaggerHtml = Read-SwaggerHtml -ApiBaseUrl $ApiBaseUrl
+$SwaggerPaths = Get-AllSwaggerFilePaths -swaggerHtml $SwaggerHtml
+
+#Loop through each version
+foreach ($SwaggerPath in $SwaggerPaths) {
+    $SwaggerSpecificationUrl = $ApiBaseUrl + $SwaggerPath
+    $SwaggerPath -match '\d' | Out-Null
+    $Version = $matches[0]
+    $ApiId = "$ApiName-v" + $Version.ToUpper()
+
+    # Get Version Set for given ApiName or create one if it does not exist
+    $VersionSet = Get-AzApiManagementApiVersionSet -Context $Context | Where-Object { $_.DisplayName -eq "$ApiName" }
+    if ($null -eq $VersionSet) {
+        $VersionSetId = (New-AzApiManagementApiVersionSet -Context $Context -Name "$ApiName" -Scheme "Header" -HeaderName "X-Version" -Description $ApiName).Id
+    }
+    else {
+        $versionSetId = $VersionSet.Id
+    }
+
+    # Import API to APIM with swagger json file
+    Import-AzApiManagementApi -Context $Context -SpecificationFormat OpenApi -ServiceUrl $ApiBaseUrl -SpecificationUrl $SwaggerSpecificationUrl -Path $ApiPath -ApiId $ApiId -ApiVersion $Version -ApiVersionSetId $VersionSetId -ErrorAction Stop -Verbose:$VerbosePreference
+
+    # Add API to Product
+    Add-AzApiManagementApiToProduct -Context $Context -ProductId $ProductId -ApiId $ApiId
+
+    # Set API Level policies
+    Set-AzApiManagementPolicy -Context $Context -ApiId $ApiId -Policy $PolicyString
+}
---
+++ Tests/UT.Import-ApimSwaggerApiDefinition.Tests.ps1
@@ -0,0 +1,52 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Unit.Tests.Config.json -Raw | ConvertFrom-Json
+Set-Location $PSScriptRoot\..\Infrastructure-Scripts\
+
+Describe "Import-ApimSwaggerApiDefinition Unit Tests" -Tags @("Unit") {
+
+    Context "Could not find APIM Instance" {
+
+        It "The specified APIM Instance was not found in the resource group, throw an error" {
+            Mock Get-AzApiManagement -MockWith { Return $null }
+            { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -ApiName $Config.apiName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId } | Should throw "APIM Instance: $($Config.instanceName) does not exist in resource group: $($Config.resourceGroupName)"
+            Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
+        }
+
+    }
+
+    Context "Could not find Swagger Page" {
+        It "Could not find main Swagger Page" {
+            Mock Get-AzApiManagement -MockWith { Return "Context" }
+            { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -ApiName $Config.apiName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId} | Should throw "Could not find swagger page at: $($Config.apiBaseUrl)/index.html"
+            Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
+        }
+    }
+
+    Context "APIM Instance exists and found swagger pages - Importing API" {
+        It "The specified Resource was not found in the subscription, throw an error" {
+            function Read-SwaggerHtml () {}
+            function Get-AllSwaggerFilePaths () {}
+            Mock Get-AzApiManagement -MockWith { Return "Context" }
+            Mock Read-SwaggerHtml -MockWith { Return $null }
+            Mock Get-AllSwaggerFilePaths -MockWith { Return @("/swagger/v1/swagger.json", "/swagger/v2/swagger.json") }
+            Mock Get-AzApiManagementApiVersionSet -MockWith {
+                return @{
+                    "Id"          = $Config.resourceId
+                    "DisplayName" = $Config.apiName
+                }
+            }
+            Mock Import-AzApiManagementApi -MockWith { Return $null }
+            Mock Add-AzApiManagementApiToProduct -MockWith { Return $null }
+            Mock Set-AzApiManagementPolicy -MockWith { Return $null }
+            { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -ApiName $Config.apiName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId } | Should not throw
+            Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Read-SwaggerHtml' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Get-AllSwaggerFilePaths' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Get-AzApiManagementApiVersionSet' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Import-AzApiManagementApi' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Add-AzApiManagementApiToProduct' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Set-AzApiManagementPolicy' -Times 1 -Scope It
+        }
+
+    }
+
+}
