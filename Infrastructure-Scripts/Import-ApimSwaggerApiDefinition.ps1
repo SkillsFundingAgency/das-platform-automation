1194d2ebf9dedd4fbcc888ce03e922e7d246499f
jack-education <38191888+jack-education@users.noreply.github.com>
2020-07-08 16:44:59 +0100

    Removed DNS request to get app service name and added sleep before swagger import (#67)

--- Infrastructure-Scripts/Import-ApimSwaggerApiDefinition.ps1
+++ Infrastructure-Scripts/Import-ApimSwaggerApiDefinition.ps1
@@ -43,15 +43,26 @@ Param(
     [String]$ProductId
 )
 
-
 function Read-SwaggerHtml ($ApiBaseUrl) {
-    try {
-        Invoke-WebRequest "$ApiBaseUrl/index.html"
+    $WebRequestRetries = 3
+    for ($i = 0; $i -lt $WebRequestRetries; ++$i) {
+        while (!$SwaggerHtml) {
+            Write-Verbose "Read-SwaggerHtml Attempt: $($i+1)"
+            try {
+                $SwaggerHtml = Invoke-WebRequest "$ApiBaseUrl/index.html"
+            }
+            catch {
+                if ($_.Exception.Message -eq "Response status code does not indicate success: 403 (Ip Forbidden)."  -and ($i+1) -ne $WebRequestRetries) {
+                    Start-Sleep -Seconds 5
+                    continue
+                }
+                else {
+                    throw "Could not find swagger page at: $ApiBaseUrl/index.html. Error: $_"
+                }
+            }
+        }
     }
-    catch {
-        throw "Could not find swagger page at: $ApiBaseUrl/index.html"
-    }
-
+    $SwaggerHtml
 }
 
 function Get-AllSwaggerFilePaths ($SwaggerHtml) {
@@ -63,9 +74,9 @@ function Get-AllSwaggerFilePaths ($SwaggerHtml) {
     $Paths
 }
 
-function Get-AppServiceName ($ApiBaseUrl){
-    $CustomHostname = $ApiBaseUrl -replace ".*https://"
-    $AppServiceName = (Resolve-DnsName -Name $CustomHostname)[0].NameHost.split('.')[0]
+function Get-AppServiceName ($ApiBaseUrl, $AppServiceResourceGroup){
+    $AppServices = Get-AzWebapp -ResourceGroupName $AppServiceResourceGroup
+    $AppServiceName = ($AppServices | Where-Object $ApiBaseUrl -like $_.hostnames | Select-Object Name).Name
     $AppServiceName
 }
 
@@ -73,7 +84,7 @@ function Add-AppServiceWhitelist ($AppServiceResourceGroup, $AppServiceName) {
     $IpRestrictions = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $AppServiceResourceGroup -Name $AppServiceName
     $MyIp = (Invoke-RestMethod ifconfig.me/ip -UseBasicParsing)
     if ($IpRestrictions.MainSiteAccessRestrictions.RuleName -notcontains "Allow all" -and ($IpRestrictions.MainSiteAccessRestrictions | Where-Object { $_.Action -eq "Allow" }).IpAddress -notcontains "$MyIp/32") {
-        Write-Output "Whitelisting $MyIp"
+        Write-Verbose "Whitelisting $MyIp"
         $Priority = ($IpRestrictions.MainSiteAccessRestrictions | Where-Object { $_.Action -eq "Allow" }).Priority[-1] + 1
         $null = Add-AzWebAppAccessRestrictionRule -ResourceGroupName $AppServiceResourceGroup -WebAppName $AppServiceName -Name "DeployServer" -IpAddress "$MyIp/32" -Priority $Priority -Action Allow
     }
@@ -81,7 +92,6 @@ function Add-AppServiceWhitelist ($AppServiceResourceGroup, $AppServiceName) {
 
 $PolicyString = "<policies><inbound><base/><authentication-managed-identity resource=`"$ApplicationIdentifierUri`"/></inbound><backend><base/></backend><outbound><base/></outbound><on-error><base/></on-error></policies>"
 
-# Check if the APIM Instance exists
 $ApimInstanceExists = Get-AzApiManagement -ResourceGroupName $ApimResourceGroup -Name $InstanceName
 if (!$ApimInstanceExists) {
     throw "APIM Instance: $InstanceName does not exist in resource group: $ApimResourceGroup"
@@ -90,24 +100,20 @@ if (!$ApimInstanceExists) {
 Write-Verbose "Building APIM context for $ApimResourceGroup\$InstanceName"
 $Context = New-AzApiManagementContext -ResourceGroupName $ApimResourceGroup -ServiceName $InstanceName
 
-# Get App Service name
-$AppServiceName = Get-AppServiceName -ApiBaseUrl $ApiBaseUrl
+$AppServiceName = Get-AppServiceName -ApiBaseUrl $ApiBaseUrl -AppServiceResourceGroup $AppServiceResourceGroup
 
-# Temporarily whitelist the deployment server
 Add-AppServiceWhitelist -AppServiceResourceGroup $AppServiceResourceGroup -AppServiceName $AppServiceName
 
-# Get all version paths
 $SwaggerHtml = Read-SwaggerHtml -ApiBaseUrl $ApiBaseUrl
 $SwaggerPaths = Get-AllSwaggerFilePaths -swaggerHtml $SwaggerHtml
 
-#Loop through each version
+Write-Verbose "Loop through each versioned Swagger definition and import to APIM"
 foreach ($SwaggerPath in $SwaggerPaths) {
     $SwaggerSpecificationUrl = $ApiBaseUrl + $SwaggerPath
     $SwaggerPath -match '\d' | Out-Null
     $Version = $matches[0]
     $ApiId = "$ApiName-v" + $Version.ToUpper()
 
-    # Get Version Set for given ApiName or create one if it does not exist
     $VersionSet = Get-AzApiManagementApiVersionSet -Context $Context | Where-Object { $_.DisplayName -eq "$ApiName" }
     if ($null -eq $VersionSet) {
         $VersionSetId = (New-AzApiManagementApiVersionSet -Context $Context -Name "$ApiName" -Scheme "Header" -HeaderName "X-Version" -Description $ApiName).Id
@@ -116,16 +122,12 @@ foreach ($SwaggerPath in $SwaggerPaths) {
         $versionSetId = $VersionSet.Id
     }
 
-    # Import API to APIM with swagger json file
     Import-AzApiManagementApi -Context $Context -SpecificationFormat OpenApi -ServiceUrl $ApiBaseUrl -SpecificationUrl $SwaggerSpecificationUrl -Path $ApiPath -ApiId $ApiId -ApiVersion $Version -ApiVersionSetId $VersionSetId -ErrorAction Stop -Verbose:$VerbosePreference
 
-    # Add API to Product
     Add-AzApiManagementApiToProduct -Context $Context -ProductId $ProductId -ApiId $ApiId
 
-    # Set API Level policies
     Set-AzApiManagementPolicy -Context $Context -ApiId $ApiId -Policy $PolicyString
 }
 
-# Remove the temporary whitelist of the deployment server
-Write-Output "Removing whitelisted IP"
+Write-Verbose "Removing whitelisted IP"
 Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $AppServiceResourceGroup -WebAppName $AppServiceName -Name "DeployServer"
