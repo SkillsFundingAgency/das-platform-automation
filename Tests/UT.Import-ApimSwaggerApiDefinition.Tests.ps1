736fadfdf18417871308864dd378465e8bc69997
jack-education <38191888+jack-education@users.noreply.github.com>
2020-08-28 01:11:53 +0100

    Fixed Import-ApimSwaggerApiDefinition retry logic

--- Tests/UT.Import-ApimSwaggerApiDefinition.Tests.ps1
+++ Tests/UT.Import-ApimSwaggerApiDefinition.Tests.ps1
@@ -15,29 +15,29 @@ Describe "Import-ApimSwaggerApiDefinition Unit Tests" -Tags @("Unit") {
 
     Context "Could not find Swagger Page" {
         It "Could not find main Swagger Page" {
-            function Get-AppServiceName () {}
-            function Add-AppServiceWhitelist () {}
+            function Get-AppServiceName () { }
+            function Add-AppServiceWhitelist () { }
             Mock Get-AzApiManagement -MockWith { Return "Context" }
             Mock Get-AppServiceName -MockWith { Return "app-service-name" }
             Mock Add-AppServiceWhitelist -MockWith { Return $null }
-            { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -AppServiceResourceGroup $Config.resourceGroupName -ApiVersionSetName $Config.apiVersionSetName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId} | Should throw "Could not find swagger page at: $($Config.apiBaseUrl)/index.html"
+            { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -AppServiceResourceGroup $Config.resourceGroupName -ApiVersionSetName $Config.apiVersionSetName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId } | Should throw "Could not find page at: $($Config.apiBaseUrl)/index.html"
             Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
         }
     }
 
     Context "APIM Instance exists and found swagger pages - Importing API" {
         It "The specified Resource was not found in the subscription, throw an error" {
-            function Get-AppServiceName () {}
-            function Add-AppServiceWhitelist () {}
-            function Read-IndexHtml () {}
-            function Get-AllSwaggerFilePaths () {}
-            function Get-ApiTitle () {}
+            function Get-AppServiceName () { }
+            function Add-AppServiceWhitelist () { }
+            function Invoke-RetryWebRequest () { }
+            function Get-AllSwaggerFilePaths () { }
+            function Get-ApiTitle () { }
             Mock Get-AzApiManagement -MockWith { Return "Context" }
             Mock Get-AppServiceName -MockWith { Return "app-service-name" }
             Mock Add-AppServiceWhitelist -MockWith { Return $null }
-            Mock Read-IndexHtml -MockWith { Return $null }
+            Mock Invoke-RetryWebRequest -MockWith { Return $null }
             Mock Get-AllSwaggerFilePaths -MockWith { Return @("/swagger/v1/swagger.json", "/swagger/v2/swagger.json") }
-            Mock Get-ApiTitle -MockWith {Return $null}
+            Mock Get-ApiTitle -MockWith { Return $null }
             Mock Get-AzApiManagementApiVersionSet -MockWith {
                 return @{
                     "Id"          = $Config.resourceId
@@ -51,7 +51,7 @@ Describe "Import-ApimSwaggerApiDefinition Unit Tests" -Tags @("Unit") {
             { ./Import-ApimSwaggerApiDefinition -ApimResourceGroup $Config.resourceGroupName -InstanceName $Config.instanceName -AppServiceResourceGroup $Config.resourceGroupName -ApiVersionSetName $Config.apiVersionSetName -ApiBaseUrl $Config.apiBaseUrl -ApiPath $Config.apiPath -ApplicationIdentifierUri $Config.applicationIdentifierUri -ProductId $Config.productId } | Should not throw
             Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-AzApiManagement' -Times 1 -Scope It
-            Assert-MockCalled -CommandName 'Read-IndexHtml' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Invoke-RetryWebRequest' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-AllSwaggerFilePaths' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-ApiTitle' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-AzApiManagementApiVersionSet' -Times 1 -Scope It
