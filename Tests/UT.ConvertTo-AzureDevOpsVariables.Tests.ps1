1675ca3091b65bcd67fac78471c40887b2c5479d
adam230594 <47275045+adam230594@users.noreply.github.com>
2020-10-12 11:47:54 +0100

    Added ConvertTo-AzureDevOpsVariables.ps1 +semver: minor (#74)
    * initial creation
    
    * fixed variables
    
    * added /
    
    * logging
    
    * fix tests
    
    * added more description
    
    * Address PR comments
    
    Co-authored-by: Mohamad Adam <accadam230594@outlook.com>

---
+++ Tests/UT.ConvertTo-AzureDevOpsVariables.Tests.ps1
@@ -0,0 +1,36 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Unit.Tests.Config.json -Raw | ConvertFrom-Json
+Set-Location $PSScriptRoot\..\Infrastructure-Scripts\
+
+Describe "ConvertTo-AzureDevOpsVariables unit tests" -Tag "Unit" {
+
+    Context "Normal string conversion" {
+        It "Should return a string correctly" {
+            $Expected = @('Creating Azure DevOps variable foo', '##vso[task.setvariable variable=foo;isOutput=true]bar')
+
+            $Output = ./ConvertTo-AzureDevOpsVariables -ARMOutput $Config.ArmOutput
+            $Output | Should be $Expected
+        }
+    }
+
+    Context "Secure string Conversion" {
+        It "Should return a securestring correctly" {
+            $Expected = @('Creating Azure DevOps variable foo',
+                '##vso[task.setvariable variable=foo;issecret=true;isOutput=true]bar')
+
+            $Output = ./ConvertTo-AzureDevOpsVariables -ARMOutput $Config.ArmOutputSecure
+            $Output | Should be $Expected
+        }
+    }
+
+
+    Context "Rename parameter" {
+        It "Should change the variable name correctly" {
+            $Expected = @('Creating Azure DevOps variable fu from foo',
+                '##vso[task.setvariable variable=fu;isOutput=true]bar')
+
+            $Output = ./ConvertTo-AzureDevOpsVariables -ARMOutput $Config.ArmOutput -Rename @{foo = "fu" }
+            $Output | Should be $Expected
+        }
+    }
+
+}
