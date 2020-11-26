e9b268c803c470862e7f3ed7bab24512725f8317
Stephen Neal <smneal@users.noreply.github.com>
2020-11-26 16:15:55 +0000

    Update so logging shows namespace output

--- Infrastructure-Scripts/Test-NamespaceExists.ps1
+++ Infrastructure-Scripts/Test-NamespaceExists.ps1
@@ -7,16 +7,16 @@ param(
 )
 
 $KubectlObject = ConvertFrom-Json -InputObject $KubectlOutput
-$Namespace = $KubectlObject.items | Where-Object { $_.metadata.name -eq $Namespace }
-if ($Namespace) { 
-    
-    Write-Verbose "Namespace $($Namespace.metadata.name) found."
-    Write-Output "##vso[task.setvariable variable=NamespaceExists]true" 
+$NamespaceSearch = $KubectlObject.items | Where-Object { $_.metadata.name -eq $Namespace }
+if ($NamespaceSearch) {
+
+    Write-Verbose "Namespace $Namespace found."
+    Write-Output "##vso[task.setvariable variable=NamespaceExists]true"
 
 }
 else {
 
     Write-Verbose "Namespace $Namespace not found."
-    Write-Output "##vso[task.setvariable variable=NamespaceExists]false" 
+    Write-Output "##vso[task.setvariable variable=NamespaceExists]false"
 
 }
