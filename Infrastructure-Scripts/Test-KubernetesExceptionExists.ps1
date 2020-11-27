db7a49a63d089d866ec97f90e601e2a9f30d83c4
Stephen Neal <smneal@users.noreply.github.com>
2020-11-27 10:48:03 +0000

    Rename namespace and add exception scripts

---
+++ Infrastructure-Scripts/Test-KubernetesExceptionExists.ps1
@@ -0,0 +1,41 @@
+<#
+    .SYNOPSIS
+    Used within an Azure DevOps pipeline as part of create-kubernetes-exception.yml to
+    test whether the resource exists in the JSON output of a 'kubectl get crd' command.
+
+    .DESCRIPTION
+    The script tests the json object passed as KubectlOutput from a Kubectl task and sets the
+    ExceptionExists Azure DevOps variable to true or false depending on if it is found.
+
+    .PARAMETER KubectlOutput
+    JSON output from kubectl task get crd command.
+
+    .PARAMETER ExceptionName
+    Exception name to test for.
+
+    .EXAMPLE
+    Test-KubernetesExceptionExists.ps1 -KubectlOutput $(PreviousPipelineTaskName.KubectlOutput) -ExceptionName 'azurepodidentityexceptions.aadpodidentity.k8s.io'
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [String]$KubectlOutput,
+    [Parameter(Mandatory=$true)]
+    [String]$ExceptionName
+)
+
+$KubectlObject = ConvertFrom-Json -InputObject $KubectlOutput
+$ExceptionSearch = $KubectlObject.items | Where-Object { $_.metadata.name -eq $ExceptionName }
+if ($ExceptionSearch) {
+
+    Write-Verbose "Exception $ExceptionName found."
+    Write-Output "##vso[task.setvariable variable=ExceptionExists]true"
+
+}
+else {
+
+    Write-Verbose "Exception $ExceptionName not found."
+    Write-Output "##vso[task.setvariable variable=ExceptionExists]false"
+
+}
