47758ca8e0bf2aee9886260754d473e997cb52d5
Stephen Neal <smneal@users.noreply.github.com>
2020-12-03 13:38:53 +0000

    Update indentation

--- Tests/UT.Test-KubernetesExceptionExists.Tests.ps1
+++ Tests/UT.Test-KubernetesExceptionExists.Tests.ps1
@@ -5,7 +5,7 @@ Describe "Test-KubernetesExceptionExists Unit Tests" -Tags @("Unit") {
 
     Context "Test whether exception CRD exists" {
         It "Should return an exception found message when the exception is found in the KubectlOutput" {
-         ./Test-KubernetesExceptionExists -KubectlOutput $Config.KubernetesExceptionReduced -ExceptionName $Config.KubernetesExceptionName | Should Contain "Exception $($Config.KubernetesExceptionName) found."
+            ./Test-KubernetesExceptionExists -KubectlOutput $Config.KubernetesExceptionReduced -ExceptionName $Config.KubernetesExceptionName | Should Contain "Exception $($Config.KubernetesExceptionName) found."
         }
 
         It "Should return an exception not found message when the exception is not found in the KubectlOutput" {
--- Tests/UT.Test-KubernetesNamespaceExists.Tests.ps1
+++ Tests/UT.Test-KubernetesNamespaceExists.Tests.ps1
@@ -5,7 +5,7 @@ Describe "Test-KubernetesNamespaceExists Unit Tests" -Tags @("Unit") {
 
     Context "Test whether namespace CRD exists" {
         It "Should return a namespace found message when the namespace is found in the KubectlOutput" {
-         ./Test-KubernetesNamespaceExists -KubectlOutput $Config.KubernetesNamespaceReduced -Namespace $Config.KubernetesNamespace | Should Contain "Namespace $($Config.KubernetesNamespace) found."
+            ./Test-KubernetesNamespaceExists -KubectlOutput $Config.KubernetesNamespaceReduced -Namespace $Config.KubernetesNamespace | Should Contain "Namespace $($Config.KubernetesNamespace) found."
         }
 
         It "Should return a namespace not found message when the namespace is not found in the KubectlOutput" {
