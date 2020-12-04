c07778615dfbd2bcf2b4df173443d0c697861b5a
NickGraham101 <nick.graham101@googlemail.com>
2020-12-04 09:28:09 +0000

    Added working version of Add-AppGatewayKubernetesIngressCert

---
+++ Infrastructure-Scripts/Add-AppGatewayKubernetesIngressCert.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param(
+    $AppGatewayName,
+    $AppGatewayResourceGroup,
+    $IngressManifestPath,
+    $KeyVaultName
+)
+
+$IngressManifestContent = Get-Content -Path $IngressManifestPath
+foreach ($Line in $IngressManifestContent) {
+    $Match = $Line -match "appgw.ingress.kubernetes.io/appgw-ssl-certificate: (.*)"
+    if ($Match) {
+        Write-Verbose "Found certificate name in ingress manifest"
+        $CertificateName = $Matches[1]
+        break
+    }
+}
+
+$AppGateway = Get-AzApplicationGateway -Name $AppGatewayName -ResourceGroupName $AppGatewayResourceGroup
+if (!(Get-AzApplicationGatewaySslCertificate -Name $CertificateName -ApplicationGateway $AppGateway -ErrorAction SilentlyContinue)) {
+    $KeyVaultCertificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName
+    $VersionLessSecretId = $KeyVaultCertificate.SecretId -replace $KeyVaultCertificate.Id, ""
+    Write-Verbose "Certificate versionless secret id is $VersionLessSecretId"
+    
+    
+    Write-Verbose "Creating app gateway ssl certificate ..."
+    $UpdatedAg = Add-AzApplicationGatewaySslCertificate -ApplicationGateway $AppGateway -Name $CertificateName -KeyVaultSecretId $VersionLessSecretId
+    Set-AzApplicationGateway -ApplicationGateway $UpdatedAg
+}
+else {
+    Write-Verbose "Certificate $CertficateName already exists, no action."
+}
+
