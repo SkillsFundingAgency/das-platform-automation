789ea7570f818d492cb6e39581dd1edb4f758c92
NickGraham101 <nick.graham101@googlemail.com>
2020-12-07 13:51:17 +0000

    Added documentation

--- Infrastructure-Scripts/Add-AppGatewayKubernetesIngressCert.ps1
+++ Infrastructure-Scripts/Add-AppGatewayKubernetesIngressCert.ps1
@@ -1,8 +1,36 @@
-﻿[CmdletBinding()]
+﻿<#
+.SYNOPSIS
+Adds a certificate to an Application Gateway from a KeyVault based on configuration contained in a Kubernetes Ingress manifest.
+
+.DESCRIPTION
+Adds a certificate to an Application Gateway from a KeyVault based on configuration contained in a Kubernetes Ingress manifest.
+
+.PARAMETER AppGatewayName
+The name of the Application Gateway the certificate will be installed on.
+
+.PARAMETER AppGatewayResourceGroup
+The Application Gateway's resource group.
+
+.PARAMETER IngressManifestPath
+The path to the Kubernetes Ingress manifest.  The Ingress should include the appgw.ingress.kubernetes.io/appgw-ssl-certificate annotation.
+Whilst the value of this annotation is arbritrary, in order for this script to work it should match the name of the certificate in the KeyVault.
+
+.PARAMETER KeyVaultName
+The name of the KeyVault that stores the certificate, must be in the same Azure subscription.
+
+.EXAMPLE
+./Add-AppGatewayKubernetesIngressCert.ps1 -AppGatewayName das-foo-ag -AppGatewayResourceGroup das-foo-rg -IngressManifestPath ./ingress.yml -KeyVaultName das-foo-kv
+
+#>
+[CmdletBinding()]
 param(
+    [Parameter(Mandatory = $true)]    
     $AppGatewayName,
+    [Parameter(Mandatory = $true)]
     $AppGatewayResourceGroup,
+    [Parameter(Mandatory = $true)]
     $IngressManifestPath,
+    [Parameter(Mandatory = $true)]
     $KeyVaultName
 )
 
@@ -21,8 +49,7 @@ if (!(Get-AzApplicationGatewaySslCertificate -Name $CertificateName -Application
     $KeyVaultCertificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName
     $VersionLessSecretId = $KeyVaultCertificate.SecretId -replace $KeyVaultCertificate.Id, ""
     Write-Verbose "Certificate versionless secret id is $VersionLessSecretId"
-    
-    
+
     Write-Verbose "Creating app gateway ssl certificate ..."
     $UpdatedAg = Add-AzApplicationGatewaySslCertificate -ApplicationGateway $AppGateway -Name $CertificateName -KeyVaultSecretId $VersionLessSecretId
     Set-AzApplicationGateway -ApplicationGateway $UpdatedAg
@@ -30,4 +57,3 @@ if (!(Get-AzApplicationGatewaySslCertificate -Name $CertificateName -Application
 else {
     Write-Verbose "Certificate $CertficateName already exists, no action."
 }
-
