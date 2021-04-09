618e22d5371045d02674fa4c14d68c3c78537583
adam230594 <47275045+adam230594@users.noreply.github.com>
2021-04-09 15:05:54 +0100

    removed redundant if condition

--- Infrastructure-Scripts/Set-AppRoleAssignments/Set-AppRoleAssignments.ps1
+++ Infrastructure-Scripts/Set-AppRoleAssignments/Set-AppRoleAssignments.ps1
@@ -109,23 +109,21 @@ try {
                 }
 
                 try {
-                    if ($ServicePrincipal.ObjectId) {
-                        $AppRoleAssignments = Get-AppRoleAssignments -ServicePrincipalId $ServicePrincipal.ObjectId
-                        $AppRoleAssignmentExists = $AppRoleAssignments.value | Where-Object { $_.appRoleId -eq $MatchedAppRole.id -and $_.principalId -eq $ManagedIdentity.ObjectId }
+                    $AppRoleAssignments = Get-AppRoleAssignments -ServicePrincipalId $ServicePrincipal.ObjectId
+                    $AppRoleAssignmentExists = $AppRoleAssignments.value | Where-Object { $_.appRoleId -eq $MatchedAppRole.id -and $_.principalId -eq $ManagedIdentity.ObjectId }
 
-                        if ($AppRoleAssignmentExists) {
-                            Write-Output "      -> App role assignment already exists"
-                            continue
+                    if ($AppRoleAssignmentExists) {
+                        Write-Output "      -> App role assignment already exists"
+                        continue
+                    }
+                    else {
+                        Write-Output "      -> Processing new app role assignment for $ResourceName with role: $($MatchedAppRole.value)"
+
+                        if (!$DryRun) {
+                            New-AppRoleAssignment -ServicePrincipalId $ServicePrincipal.ObjectId -AppRoleId $MatchedAppRole.id -ManagedIdentity $ManagedIdentity.ObjectId
                         }
-                        else {
-                            Write-Output "      -> Processing new app role assignment for $ResourceName with role: $($MatchedAppRole.value)"
 
-                            if (!$DryRun) {
-                                New-AppRoleAssignment -ServicePrincipalId $ServicePrincipal.ObjectId -AppRoleId $MatchedAppRole.id -ManagedIdentity $ManagedIdentity.ObjectId
-                            }
-
-                            Write-Output "      -> Successfully created app role assignment"
-                        }
+                        Write-Output "      -> Successfully created app role assignment"
                     }
                 }
                 catch {
