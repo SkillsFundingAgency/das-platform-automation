60e0ff18a3b704c7851cfda14f254dac263a3865
jack-cloud-platform <38191888+jack-cloud-platform@users.noreply.github.com>
2021-08-10 22:07:38 +0100

    Converted array to JSON

--- Infrastructure-Scripts/Set-BackEndAccessRestrictionsVariable.ps1
+++ Infrastructure-Scripts/Set-BackEndAccessRestrictionsVariable.ps1
@@ -68,5 +68,9 @@ foreach ($BackEndAccessRestrictionObject in $BackEndAccessRestrictionsArray) {
 
 $BackEndAccessRestrictionsArray
 
-Write-Output "Setting value of BackEndAccessRestrictionsArray variable to BackEndAccessRestrictions pipeline variable"
-Write-Output "##vso[task.setvariable variable=BackEndAccessRestrictions]$BackEndAccessRestrictionsArray"
+$BackEndAccessRestrictionsArrayJson = Convert-ToJson $BackEndAccessRestrictionsArray
+
+$BackEndAccessRestrictionsArrayJson
+
+Write-Output "Setting value of BackEndAccessRestrictionsArrayJson variable to BackEndAccessRestrictions pipeline variable"
+Write-Output "##vso[task.setvariable variable=BackEndAccessRestrictions]$BackEndAccessRestrictionsArrayJson"
