8d45fb32db7f81303a09e3dd7cc7dc12012dae85
EVANS <Christian.EVANS@EDUCATION.GOV.UK>
2021-10-28 17:28:19 +0100

    trouble shooting error with token - added token directly into header of request

--- Infrastructure-Scripts/Get-EnvironmentDeploymentRecords.ps1
+++ Infrastructure-Scripts/Get-EnvironmentDeploymentRecords.ps1
@@ -15,11 +15,12 @@ Param (
 )
 
 $Url = "https://dev.azure.com/$Organisation/$Project/_apis/distributedtask/environments/$EnvironmentId/environmentdeploymentrecords?top=100?api-version=6.0-preview.1"
-$header = @{authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"}
 
 while ($true){
     #Invoke call to Azure DevOps Rest API to get all build data for given environment.
-    $EnvironmentPipelineRuns = (Invoke-RestMethod -Method GET -Uri $Url -Headers $header).value
+    $EnvironmentPipelineRuns = (Invoke-RestMethod -Method GET -Uri $Url -Headers  @{
+        Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"
+    }).value
     #Filter down results of API call to just contain relevant pipeline runs with matching Pipeline names and only ones that are still running.
     $RunningEnvironmentPipelineRuns = $EnvironmentPipelineRuns | where-object {$_.definition.name -eq $PipelineName -and -not $_.result}
     $LowestRunId = ($RunningEnvironmentPipelineRuns.owner.id | Sort-Object)[0]
