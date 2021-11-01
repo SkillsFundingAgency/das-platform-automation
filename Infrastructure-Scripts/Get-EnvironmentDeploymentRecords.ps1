87e11409039bfc7ea5ef6a3f34763aa9132aeed6
EVANS <Christian.EVANS@EDUCATION.GOV.UK>
2021-11-01 11:13:37 +0000

    made script call incorrect URl and added try catch to attempt to drop out of run and carry on depoyment

--- Infrastructure-Scripts/Get-EnvironmentDeploymentRecords.ps1
+++ Infrastructure-Scripts/Get-EnvironmentDeploymentRecords.ps1
@@ -10,20 +10,25 @@ Param (
     [Int]$SleepTime = 20
 )
 
-$Url = "$env:SYSTEM_ORGANISATIONNAME/$env:SYSTEM_PROJECTNAME/_apis/distributedtask/environments/$EnvironmentId/environmentdeploymentrecords?top=100?api-version=6.0-preview.1"
+$Url = test#"$env:SYSTEM_ORGANISATIONNAME/$env:SYSTEM_PROJECTNAME/_apis/distributedtask/environments/$EnvironmentId/environmentdeploymentrecords?top=100?api-version=6.0-preview.1"
 
-while ($true){
-    #Invoke call to Azure DevOps Rest API to get all build data for given environment.
-    $EnvironmentPipelineRuns = (Invoke-RestMethod -Method GET -Uri $Url -Headers  @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }).value
-    #Filter down results of API call to just contain relevant pipeline runs with matching Pipeline names and only ones that are still running.
-    $RunningEnvironmentPipelineRuns = $EnvironmentPipelineRuns | where-object {$_.definition.name -eq $PipelineName -and -not $_.result}
-    $LowestRunId = ($RunningEnvironmentPipelineRuns.owner.id | Sort-Object)[0]
-    if ($RunId -eq $LowestRunId) {
-        Write-Host("Continuing with deployment.")
-        break;
+try{
+    while ($true){
+        #Invoke call to Azure DevOps Rest API to get all build data for given environment.
+        $EnvironmentPipelineRuns = (Invoke-RestMethod -Method GET -Uri $Url -Headers  @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }).value
+        #Filter down results of API call to just contain relevant pipeline runs with matching Pipeline names and only ones that are still running.
+        $RunningEnvironmentPipelineRuns = $EnvironmentPipelineRuns | where-object {$_.definition.name -eq $PipelineName -and -not $_.result}
+        $LowestRunId = ($RunningEnvironmentPipelineRuns.owner.id | Sort-Object)[0]
+        if ($RunId -eq $LowestRunId) {
+            Write-Host("Continuing with deployment.")
+            break;
+        }
+        else {
+            Start-Sleep -s $SleepTime
+            Write-Host("There is another deployment to this stage currently running in this environment. Retrying in $SleepTime seconds")
+        }
     }
-    else {
-        Start-Sleep -s $SleepTime
-        Write-Host("There is another deployment to this stage currently running in this environment. Retrying in $SleepTime seconds")
-    }
+}
+catch{
+    break;
 }
