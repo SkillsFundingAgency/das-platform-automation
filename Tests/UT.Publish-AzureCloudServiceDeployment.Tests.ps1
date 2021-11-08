b0dc50d289ca08d7e05463501518fb34899c54f7
jack-cloud-platform <38191888+jack-cloud-platform@users.noreply.github.com>
2021-11-08 11:03:02 +0000

    Added UT.Publish-AzureCloudServiceDeployment.Tests.ps1

---
+++ Tests/UT.Publish-AzureCloudServiceDeployment.Tests.ps1
@@ -0,0 +1,23 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Configuration\Unit.Tests.Config.json -Raw | ConvertFrom-Json
+Set-Location $PSScriptRoot\..\Infrastructure-Scripts
+
+Describe "Publish-AzureCloudServiceDeployment Unit Tests" -Tags @("Unit") {
+    Context "Cloud service does not exist, should not throw an error" {
+        It "Cloud service does not exist, should not throw an error" {
+            Mock Get-AzureSubscription -MockWith { 
+                return @{
+                    SubscriptionId = $Config.guid
+                }
+            }
+            Mock Set-AzureSubscription -MockWith { return $null }
+            Mock Get-AzureService -MockWith { return $null }
+            Mock Invoke-Expression -MockWith { return $null }
+            Mock Get-AzureDeployment -MockWith { return $null }
+            { .\Publish-AzureCloudServiceDeployment.ps1 -ServiceName das-at-foobar-cs -ServiceLocation foobar -ClassicStorageAccountName dasatfoobarstr -ServicePackageFile ./SFA.DAS.FooBar.CloudService.cspkg -ServiceConfigFile ./ServiceConfiguration.Cloud.cscfg -Slot Production } | Should not throw
+            Assert-MockCalled -CommandName 'Get-AzureSubscription' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Set-AzureSubscription' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Get-AzureService' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Invoke-Expression' -Times 1 -Scope It
+        }
+    }
+}
