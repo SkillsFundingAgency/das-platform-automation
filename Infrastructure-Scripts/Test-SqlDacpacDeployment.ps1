8e6eacde6e75375bc2017a8473fa5613ef3d8aa3
NickGraham101 <nick.graham101@googlemail.com>
2021-11-08 13:24:40 +0000

    Added log line

--- Infrastructure-Scripts/Test-SqlDacpacDeployment.ps1
+++ Infrastructure-Scripts/Test-SqlDacpacDeployment.ps1
@@ -25,6 +25,7 @@ if ($OverrideBlockOnPossibleDataLoss) {
         catch {
             throw "ApproveOverrideBlockOnPossibleDataLoss variable is not set in this pipeline.  See docs for this script for further info."
         }
+        Write-Verbose "Override for BlockOnPossibleDataLoss approved, setting AdditionalArgument '/p:BlockOnPossibleDataLoss=false'"
     }
     else {
         Write-Verbose "Environment is not PROD, overriding BlockOnPossibleDataLoss"
