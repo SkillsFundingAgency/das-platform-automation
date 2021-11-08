c88da3ca84376fc6a8964d6d07d35ea94a0168cf
NickGraham101 <nick.graham101@googlemail.com>
2021-11-08 10:34:57 +0000

    Initial version of Test-SqlDacpacDeployment script

---
+++ Infrastructure-Scripts/Test-SqlDacpacDeployment.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [VariableSet("AT", "TEST", "TEST2", "PP", "PROD", "MO", "DEMO")]
+    [string]$Environment
+    [Parameter(Mandatory=$true)]
+    [boolean]$OverrideBlockOnPossibleDataLoss
+)
+
+Write-Verbose "Checking if BlockOnPossibleDataLoss has been overridden"
+if ($OverrideBlockOnPossibleDataLoss) {
+    Write-Verbose "Override BlockOnPossibleDataLoss requested"
+    if ($Environment -eq "PROD") {
+        Write-Verbose "Environment is PROD, checking for approval to override BlockOnPossibleDataLoss"
+    }
+    else {
+        Write-Verbose "Environment is not PROD, overriding BlockOnPossibleDataLoss"
+    }
+}
+else {
+    Write-Verbose "Override BlockOnPossibleDataLoss not requested"
+}
+
