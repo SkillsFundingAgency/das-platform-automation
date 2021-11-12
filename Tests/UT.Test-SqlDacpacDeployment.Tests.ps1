9a44fef76836fa911c9741bea7bbf8c770b0a19b
EVANS <Christian.EVANS@EDUCATION.GOV.UK>
2021-11-12 12:33:39 +0000

    WIP on testing still

--- Tests/UT.Test-SqlDacpacDeployment.Tests.ps1
+++ Tests/UT.Test-SqlDacpacDeployment.Tests.ps1
@@ -6,14 +6,49 @@ Describe "Test-SqlDacpacDeployment Unit Tests" -Tags @("Unit") {
         Environment = "PROD"
         OverrideBlockOnPossibleDataLoss = $true
     }
+    $ParamsNoOverridePROD = @{
+        Environment = "PROD"
+        OverrideBlockOnPossibleDataLoss = $false
+    }
+    $ParamsOverrideNonPROD = @{
+        Environment = "AT"
+        OverrideBlockOnPossibleDataLoss = $true
+    }
+    $ParamsNoOverrideNonPROD = @{
+        Environment = "AT"
+        OverrideBlockOnPossibleDataLoss = $false
+    }
 
-    $ENV:ApproveProdOverrideBlockOnPossibleDataLoss = $true
-
-    Context "Test-SqlDacpacDeployment runs with Override for BlockOnPossibleDataLoss set to true and in PROD environment" {
-        It "Script is ran with OverrideBlockOnPossibleDataLoss set to true within PROD environment. The following message is given - 'Override for BlockOnPossibleDataLoss approved, setting AdditionalArgument '/p:BlockOnPossibleDataLoss=false.'" {
+    Context "Test-SqlDacpacDeployment runs with override for BlockOnPossibleDataLoss set to true and in PROD environment" {
+        $ENV:ApproveProdOverrideBlockOnPossibleDataLoss = $true
+        It "Script is ran with OverrideBlockOnPossibleDataLoss set to true within PROD environment. The following message is given - 'Override for BlockOnPossibleDataLoss approved, setting AdditionalArgument '/p:BlockOnPossibleDataLoss=false.''" {
             ./Test-SqlDacpacDeployment @ParamsOverridePROD | Should not Throw
             Assert-MockCalled Write-Verbose -Times 1 -Scope It -ParameterFilter { $InputObject -match "Override for BlockOnPossibleDataLoss approved, setting AdditionalArgument '/p:BlockOnPossibleDataLoss=false.'" }
-            #Assert-MockCalled Write-Output -Times 1 -Scope It -ParameterFilter { $InputObject -match "##vso[task.setvariable variable=SetBlockOnPossibleDataLossArgument]true" }
+            Assert-MockCalled Write-Output -Times 1 -Scope It -ParameterFilter { $InputObject -match "##vso[task.setvariable variable=SetBlockOnPossibleDataLossArgument]true" }
+        }
+    }
+    Context "Test-SqlDacpacDeployment runs with no override for BlockOnPossibleDataLoss and in PROD environment" {
+        $ENV:ApproveProdOverrideBlockOnPossibleDataLoss = $false
+        It "Script is ran with OverrideBlockOnPossibleDataLoss set to true within PROD environment. The following message is given - 'Override for BlockOnPossibleDataLoss not approved, deploying DACPAC with default arguments'" {
+            ./Test-SqlDacpacDeployment @ParamsNoOverridePROD | Should not Throw
+            Assert-MockCalled Write-Verbose -Times 1 -Scope It -ParameterFilter { $InputObject -match "Override for BlockOnPossibleDataLoss not approved, deploying DACPAC with default arguments" }
+            Assert-MockCalled Write-Output -Times 1 -Scope It -ParameterFilter { $InputObject -match "##vso[task.setvariable variable=SetBlockOnPossibleDataLossArgument]false" }
+        }
+    }
+    Context "Test-SqlDacpacDeployment runs with override for BlockOnPossibleDataLoss set to true and in AT environment" {
+        $ENV:ApproveProdOverrideBlockOnPossibleDataLoss = $false
+        It "Script is ran with OverrideBlockOnPossibleDataLoss set to true within PROD environment. The following message is given - 'Environment is not PROD, overriding BlockOnPossibleDataLoss'" {
+            ./Test-SqlDacpacDeployment @ParamsNoOverridePROD | Should not Throw
+            Assert-MockCalled Write-Verbose -Times 1 -Scope It -ParameterFilter { $InputObject -match "Environment is not PROD, overriding BlockOnPossibleDataLoss" }
+            Assert-MockCalled Write-Output -Times 1 -Scope It -ParameterFilter { $InputObject -match "##vso[task.setvariable variable=SetBlockOnPossibleDataLossArgument]true" }
+        }
+    }
+    Context "Test-SqlDacpacDeployment runs with override for BlockOnPossibleDataLoss set to true and in AT environment" {
+        $ENV:ApproveProdOverrideBlockOnPossibleDataLoss = $false
+        It "Script is ran with OverrideBlockOnPossibleDataLoss set to true within PROD environment. The following message is given - 'Override BlockOnPossibleDataLoss not requested'" {
+            ./Test-SqlDacpacDeployment @ParamsNoOverrideNonPROD | Should not Throw
+            Assert-MockCalled Write-Verbose -Times 1 -Scope It -ParameterFilter { $InputObject -match "Override BlockOnPossibleDataLoss not requested" }
+            Assert-MockCalled Write-Output -Times 1 -Scope It -ParameterFilter { $InputObject -match "##vso[task.setvariable variable=SetBlockOnPossibleDataLossArgument]false" }
         }
     }
 }
