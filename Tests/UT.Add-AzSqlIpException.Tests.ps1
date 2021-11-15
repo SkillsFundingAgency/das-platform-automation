90c161beae02a481260503be33a79aedca8805f5
Nathan Bowes <nathan.bowes@education.gov.uk>
2021-11-15 16:15:59 +0000

    Add missing UT Commit

--- Tests/UT.Add-AzSqlIpException.Tests.ps1
+++ Tests/UT.Add-AzSqlIpException.Tests.ps1
@@ -3,12 +3,17 @@ Set-Location $PSScriptRoot\..\Infrastructure-Scripts\
 
 Describe "Add-AzSqlIpException Unit Tests" -Tags @("Unit") {
 
-    $env:RELEASE_REQUESTEDFOR =  $Config.ruleName
+    $env:RELEASE_REQUESTEDFOR = $Config.ruleName
+    $Params = @{
+        IpAddress           = $Config.ipAddress
+        ResourceNamePattern = $Config.ResourceName
+        Name                = "TestUser"
+    }
 
     Context "Resource does not exist" {
         It "The specified Resource was not found in the subscription, throw an error" {
             Mock Get-AzResource -MockWith { return $null }
-            { ./Add-AzSqlIpException -IpAddress $Config.ipAddress -ResourceNamePattern $Config.resourceName } | Should throw "Failed to add firewall exception: Could not find a resource matching $($Config.resourceName) in the subscription"
+            { ./Add-AzSqlIpException @Params } | Should throw "Failed to add firewall exception: Could not find a resource matching $($Config.resourceName) in the subscription"
             Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
         }
     }
@@ -22,12 +27,13 @@ Describe "Add-AzSqlIpException Unit Tests" -Tags @("Unit") {
                     "Name"              = $Config.serverName
                 }
             }
-            Mock Get-AzSqlServerFirewallRule -MockWith { return @{
-                    "FirewallRuleName" = $Config.ruleName
+            Mock Get-AzSqlServerFirewallRule -MockWith {
+                return @{
+                    "FirewallRuleName" = "TestUser"
                 }
             }
             Mock Set-AzSqlServerFirewallRule -MockWith { return $null }
-            { ./Add-AzSqlIpException -IpAddress $Config.ipAddress -ResourceNamePattern $Config.resourceName } | Should Not throw
+            { ./Add-AzSqlIpException @Params } | Should Not throw
             Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-AzSqlServerFirewallRule' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Set-AzSqlServerFirewallRule' -Times 1 -Scope It
@@ -45,7 +51,7 @@ Describe "Add-AzSqlIpException Unit Tests" -Tags @("Unit") {
             }
             Mock Get-AzSqlServerFirewallRule -MockWith { return @() }
             Mock New-AzSqlServerFirewallRule -MockWith { return $null }
-            { ./Add-AzSqlIpException -IpAddress $Config.ipAddress -ResourceNamePattern $Config.resourceName } | Should Not throw
+            { ./Add-AzSqlIpException @Params } | Should Not throw
             Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'Get-AzSqlServerFirewallRule' -Times 1 -Scope It
             Assert-MockCalled -CommandName 'New-AzSqlServerFirewallRule' -Times 1 -Scope It
