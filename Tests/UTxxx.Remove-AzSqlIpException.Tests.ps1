7f9eaafdc63e283fed18a13392648fc1bf3ed8f7
Kudzai Nyatsine <kudzai.nyatsine@outlook.com>
2021-11-23 11:33:22 +0000

    Modified unit test

--- Tests/UTxxx.Remove-AzSqlIpException.Tests.ps1
+++ Tests/UTxxx.Remove-AzSqlIpException.Tests.ps1
@@ -5,16 +5,28 @@ Describe "Remove-AzSqlIpException Unit Tests" -Tags @("Unit") {
 
     $env:RELEASE_REQUESTEDFOR = $Config.ruleName
     $Params = @{
-        IpAddress           = $Config.ipAddress
-        ResourceName = $Config.resourceName
-        Name                = "TestUser"
+        StartIpAddress = $Config.ipAddress
+        EndIPAddress   = $Config.ipAddress
+        servername     = $Config.servername
+        Name           = "TestUser"
     }
-    Context "Resource does not exist" {
-        It "The specified Resource was not found in the subscription, throw an error" {
-            Mock Get-AzResource -MockWith { return $null }
-            { ./Remove-AzSqlIpException @Params } | Should throw "Failed to add firewall exception: Could not find a resource matching $($Config.resourceName) in the subscription"
-            Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
+
+    Context "Remove firewall rulename on a sql server" {
+        It "Should remove firewall rule on the given server name" {
+
+            Mock Get-AzSqlServerFirewallRule -MockWith {
+                return @{
+                    "ResourceGroupName" = $Config.resourceGroupName
+                    "Servername"        = $Config.serverName
+                    "FirewallRuleName"  = $Config.rulename
+                }
+            }
         }
+        Mock Remove-AzSqlServerFirewallRule -MockWith { return $null }
+        { ./Remove-AzSqlIpException @Params } | Should Not throw
+        Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
+        Assert-MockCalled -CommandName 'Get-AzSqlServerFirewallRule' -Times 1 -Scope It
+        Assert-MockCalled -CommandName 'Remove-AzSqlServerFirewallRule' -Times 1 -Scope It
     }
 }
 
