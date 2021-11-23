ddfe7e1ad28b71c9198a81158c5765e8fffd32c4
Kudzai Nyatsine <kudzai.nyatsine@outlook.com>
2021-11-23 11:11:20 +0000

    checking unit test

--- Tests/UTxxx.Remove-AzSqlIpException.Tests.ps1
+++ Tests/UTxxx.Remove-AzSqlIpException.Tests.ps1
@@ -6,14 +6,13 @@ Describe "Remove-AzSqlIpException Unit Tests" -Tags @("Unit") {
     $env:RELEASE_REQUESTEDFOR = $Config.ruleName
     $Params = @{
         IpAddress           = $Config.ipAddress
-        serverName = $Config.serverName
+        ResourceName = $Config.resourceName
         Name                = "TestUser"
     }
-
-    Context "SQL Server does not exist" {
-        It "The specified resource was not found in the subscription, throw an error" {
+    Context "Resource does not exist" {
+        It "The specified Resource was not found in the subscription, throw an error" {
             Mock Get-AzResource -MockWith { return $null }
-            { ./Remove-AzSqlIpException @Params } | Should throw "Failed to add firewall exception: Could not find a server matching $($Config.server) in the subscription"
+            { ./Remove-AzSqlIpException @Params } | Should throw "Failed to add firewall exception: Could not find a resource matching $($Config.resourceName) in the subscription"
             Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
         }
     }
