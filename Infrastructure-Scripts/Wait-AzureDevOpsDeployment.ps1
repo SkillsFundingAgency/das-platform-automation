baa2e82f8f951afac8a65758dfaec4a753877b7d
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2021-12-01 14:14:34 +0000

    Dasd 9249 improve wait az dev ops deployment script (#100)
    * Parameterised retry limit with default of 30
    
    * Script throws is retry limit reached
    
    * Added logging
    
    * Refactored to accept PAT token and remove some environment variable references
    
    * Removed unneeded slash from url
    
    * Updated tests
    
    * Updated docs
    
    * Implemented logic to handle continuation token
    
    * Fixed query string in url, increased requested items and updated api-version
    
    * Improvements to docs
    
    * Refined logging
    
    * Removed hardcoding
    
    * Fixed spelling of variable name

--- Infrastructure-Scripts/Wait-AzureDevOpsDeployment.ps1
+++ Infrastructure-Scripts/Wait-AzureDevOpsDeployment.ps1
@@ -4,6 +4,13 @@
 
     .DESCRIPTION
     Gets payload from Azure DevOps RestAPI (https://docs.microsoft.com/en-us/rest/api/azure/devops/distributedtask/environmentdeployment-records/list?view=azure-devops-rest-6.0) and filters it to produce the build ID of the newest running build. Then compares to its own build ID, if equal to it then run and if not sleep and redo.
+    Relies on System.AccessToken when running in a pipeline, ensure this is mapped as described here - https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#systemaccesstoken
+
+    .PARAMETER AzureDevOpsOrganisationUri
+    The URI of the Azure DevOps organisation the pipeline is running in, typically passed in using the predefined variable System.CollectionUri (https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#system-variables-devops-services)
+
+    .PARAMETER AzureDevOpsProjectName
+    The name (or id) of the Azure DevOps project the pipeline is defined in, best passed in using the predefined variable System.TeamProjectId
 
     .PARAMETER EnvironmentId
     The ID number associated with the environemnt stage being used for the deployment.
@@ -14,8 +21,14 @@
     .PARAMETER RunId
     The Run ID of the current deployment which is used to compare to the lowest active RunID returned by the API.
 
+    .PARAMETER RetryLimit
+    (optional) The number of times the process will retry the comparison if there is another deployment running, defaults to 30.
+
     .PARAMETER SleepTime
-    The amount of time in seconds that the process will wait before retrying the comparison if there is another deployment currently running.
+    (optional) The amount of time in seconds that the process will wait before retrying the comparison if there is another deployment currently running, defaults to 20 seconds.
+
+    .PARAMETER PatToken
+    (optional) A PAT token for local debugging
 
     .EXAMPLE
     .\Wait-AzureDevOpsDeployment.ps1 -EnvironmentId 139 -PipelineName das-levy-transfer-matching-api -RunId 459282
@@ -24,42 +37,73 @@
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory = $true)]
+    [String]$AzureDevOpsOrganisationUri,
+    [Parameter(Mandatory = $true)]
+    [String]$AzureDevOpsProjectName,
+    [Parameter(Mandatory = $true)]
     [String]$EnvironmentId,
     [Parameter(Mandatory = $true)]
     [String]$PipelineName,
     [Parameter(Mandatory = $true)]
     [Int]$RunId,
     [Parameter(Mandatory = $false)]
-    [Int]$SleepTime = 20
+    [Int]$RetryLimit = 30,
+    [Parameter(Mandatory = $false)]
+    [Int]$SleepTime = 20,
+    [Parameter(Mandatory = $false)]
+    [string]$PatToken
 )
 
-$Url = "$env:SYSTEM_ORGANISATIONNAME/$env:SYSTEM_PROJECTNAME/_apis/distributedtask/environments/$EnvironmentId/environmentdeploymentrecords?top=100?api-version=6.0-preview.1"
+$Url = "$AzureDevOpsOrganisationUri$AzureDevOpsProjectName/_apis/distributedtask/environments/$EnvironmentId/environmentdeploymentrecords?top=500&api-version=6.1-preview.1"
 $RetryCounter = 0
 
-while ($RetryCounter -lt 30){
+if ($PatToken) {
+    $Base64PatToken = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PatToken)"))
+    $Headers = @{ Authorization = "Basic $Base64PatToken" }
+}
+else {
+    $Headers = @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
+}
+
+while ($RetryCounter -lt $RetryLimit) {
     Write-Verbose "Attempt $RetryCounter"
+    $EnvironmentDeployments = @()
     try{
         #Invoke call to Azure DevOps Rest API to get all deployment data for given environment.
-        $EnvironmentDeployments = Invoke-RestMethod -Method GET -Uri $Url -Headers  @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" } -TimeoutSec 30
+        Write-Verbose "Requesting: $Url"
+        $EnvironmentDeployments += (Invoke-RestMethod -Method GET -Uri $Url -Headers $Headers -TimeoutSec 30 -ResponseHeadersVariable ResponseHeaders).value
+        while ($ResponseHeaders.'x-ms-continuationtoken') {
+            $ContinuationUrl = "$Url&continuationToken=$($ResponseHeaders.'x-ms-continuationtoken')"
+            $EnvironmentDeployments += (Invoke-RestMethod -Method GET -Uri $ContinuationUrl -Headers $Headers -TimeoutSec 30 -ResponseHeadersVariable ResponseHeaders).value
+            if ($ResponseHeaders.'Retry-After') {
+                Write-Warning "Request is being rate limited"
+                ##TO DO: implement logic to handle rate limiting responses - https://docs.microsoft.com/en-us/azure/devops/integrate/concepts/rate-limits?view=azure-devops#api-client-experience
+            }
+        }
     }
     catch{
         Write-Error "Response code $($_.Exception.Response.StatusCode.Value__) received. Terminating process, deployment will continue."
         Write-Output $_
         break;
     }
-    #Filter down results of API call to just contain relevant pipeline runs with matching Pipeline names and only ones that are still running.
-    $RunningEnvironmentDeployments = $EnvironmentDeployments.value | Where-Object {$_.definition.name -eq $PipelineName -and -not $_.result}
+
+    #Filter down results of API call to just contain relevant pipeline runs with matching Pipeline names and only ones that are still running.  This does not include queued jobs.
+    $RunningEnvironmentDeployments = $EnvironmentDeployments | Where-Object {$_.definition.name -eq $PipelineName -and -not $_.result}
+    Write-Host "Running jobs:`n$($RunningEnvironmentDeployments | Select-Object -Property startTime, @{ label="pipelineName"; expression={$_.definition.name}}, result, id, stageName, jobName, @{label="ownerName";expression={$_.owner.name}}, @{label="ownerId";expression={$_.owner.id}}, planId | Where-Object { $_.pipelineName -eq $PipelineName } | Format-Table | Out-String)"
     $LowestRunId = $RunningEnvironmentDeployments.owner.id | Sort-Object -Top 1
+    Write-Output "Lowest run id is $LowestRunId, current run id is $RunId"
     if ($RunId -eq $LowestRunId) {
         Write-Output("Continuing with deployment.")
         break;
     }
     else {
+        Write-Output "There is another deployment to this stage currently running in this environment. Retrying in $SleepTime seconds."
         $RetryCounter++
         Start-Sleep -Seconds $SleepTime
-        Write-Output("There is another deployment to this stage currently running in this environment. Retrying in $SleepTime seconds.")
     }
 }
-if ($RetryCounter -eq 30) {
-    Write-Output("Retry limit has been reached - Continuing with deployment.")
+
+if ($RetryCounter -ge $RetryLimit) {
+    Write-Warning "Retry limit has been reached, terminating deployment - please retry later"
+    throw "Deployment timed out due to other deployments in progress"
 }
