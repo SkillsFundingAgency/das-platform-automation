282e39377bd01c5d23d9c3768da155b921c2f6bb
NickGraham101 <nick.graham101@googlemail.com>
2022-06-07 14:40:14 +0100

    Processes all apps in a subscription using a filter

--- Infrastructure-Scripts/Set-AzAppServiceHttp2State.ps1
+++ Infrastructure-Scripts/Set-AzAppServiceHttp2State.ps1
@@ -3,7 +3,8 @@
     Checks the status of the HTTP/2 setting for app services and sets to true.
 
     .DESCRIPTION
-    Checks the status of the HTTP/2 setting for app services and sets to true.
+    Checks the status of the HTTP/2 setting for app services and sets to true.  Can select a subset of all the app services for a subsciption using AppServiceNamePrefix or be passed
+    the name and resource group for a single app service.
 
     .PARAMETER AppServiceName
     The name of the app service
@@ -11,30 +12,52 @@
     .PARAMETER AppServiceResourceGroup
     The resource group containing the app service
 
+    .PARAMETER AppServiceNamePrefix
+    A filter to select a subset of the app services in a subscription
+
     .EXAMPLE
     To see whether a change would be made to a single app service
     Set-AzAppServiceHttp2State.ps1 -AppServiceName das-at-crsdelapi-as -AppServiceResourceGroup das-at-crsdel-rg -WhatIf
 
+    .EXAMPLE
+    To see which app services would be changed in a subscription
+    Set-AzAppServiceHttp2State.ps1 AppServiceNamePrefix "das-at-" -WhatIf
 #>
 
 [CmdletBinding(DefaultParameterSetName =  "None", SupportsShouldProcess = $true)]
 param(
-    [Parameter(Mandatory = $true, ParameterSetName="SingleAppService")]
-    $AppServiceName,
-    [Parameter(Mandatory = $true, ParameterSetName="SingleAppService")]
-    $AppServiceResourceGroup
+    [Parameter(Mandatory=$true, ParameterSetName="SingleAppService")]
+    [string]$AppServiceName,
+    [Parameter(Mandatory=$true, ParameterSetName="SingleAppService")]
+    [string]$AppServiceResourceGroup,
+    [Parameter(Mandatory=$true, ParameterSetName="Subscription")]
+    [string]$AppServiceNamePrefix
 )
 
-$Resource = Get-AzResource -Name $AppServiceName -ResourceGroupName $AppServiceResourceGroup -ResourceType Microsoft.Web/sites
-
-if ($Resource.Properties.siteConfig.http20Enabled) {
-    Write-Verbose "HTTP/2 is enabled for AppServiceName"
+Begin {
+    if ($PSCmdlet.ParameterSetName -eq "Subscription") {
+        $AppServices = Get-AzWebApp | Where-Object { $_.Name -match "^$AppServiceNamePrefix.+" }
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "SingleAppService") {
+        $AppService = New-Object -TypeName PSCustomObject -Property @{ Name = $AppServiceName; ResourceGroup = $AppServiceResourceGroup }
+        $AppServices = @($AppService)
+    }
 }
-else {
-    Write-Verbose "HTTP/2 is not enabled for AppServiceName, enabling ..."
 
-    if ($PSCmdlet.ShouldProcess($Resource.Name, "Setting HTTP/2 to enabled")) {
-        $Resource.Properties.siteConfig.http20Enabled = $true
-        $Resource | Set-AzResource -Force | Out-Null
+Process {
+    foreach ($AppService in $AppServices) {
+        $Resource = Get-AzResource -Name $AppService.Name -ResourceGroupName $AppService.ResourceGroup -ResourceType Microsoft.Web/sites
+
+        if ($Resource.Properties.siteConfig.http20Enabled) {
+            Write-Verbose "HTTP/2 is enabled for $($AppService.Name)"
+        }
+        else {
+            Write-Verbose "HTTP/2 is not enabled for $($AppService.Name), enabling ..."
+
+            if ($PSCmdlet.ShouldProcess($Resource.Name, "Setting HTTP/2 to enabled")) {
+                $Resource.Properties.siteConfig.http20Enabled = $true
+                $Resource | Set-AzResource -Force | Out-Null
+            }
+        }
     }
 }
