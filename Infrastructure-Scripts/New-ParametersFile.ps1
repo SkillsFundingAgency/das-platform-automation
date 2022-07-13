7f508d6f53e5566edeb35ae10d800dcc72ba20f3
Nathan Bowes <nathan.bowes@education.gov.uk>
2022-07-13 11:51:07 +0100

    Secret testing

--- Infrastructure-Scripts/New-ParametersFile.ps1
+++ Infrastructure-Scripts/New-ParametersFile.ps1
@@ -48,15 +48,23 @@ foreach ($ParameterObject in $ParameterObjects) {
         $ParameterName = $ParameterObject.Name.ToUpper()
         $ParameterValue = (Get-Item -Path "env:$ParameterName" -ErrorAction SilentlyContinue).Value
         if (!$ParameterValue){
-            Write-Host -Message "Environment variable for $ParameterName was not found, attempting default value"
-            if ($null -eq $ParameterObject.Value.defaultValue) {
-                Write-Verbose -Message "Default value for $ParameterName was not found. Process will terminate"
-                throw "Could not find environment variable or default value for template parameter $ParameterName"
+            Write-Host -Message "Environment variable for $ParameterName was not found, attempting secret value"
+            $ParameterValue = (Get-Item -Path "env:SECRET_$ParameterName" -ErrorAction SilentlyContinue).Value
+            if (!$ParameterValue) {
+                Write-Host -Message "Environment variable for $ParameterName was not found, attempting default value"
+                if ($null -eq $ParameterObject.Value.defaultValue) {
+                    Write-Verbose -Message "Default value for $ParameterName was not found. Process will terminate"
+                    throw "Could not find environment variable or default value for template parameter $ParameterName"
+                }
+                else {
+                    Write-Verbose -Message "Parameter $ParameterName has a default value, skipping this parameter"
+                    continue
+                }
             }
             else {
-                Write-Verbose -Message "Parameter $ParameterName has a default value, skipping this parameter"
                 continue
             }
+
         }
         else {
             continue
