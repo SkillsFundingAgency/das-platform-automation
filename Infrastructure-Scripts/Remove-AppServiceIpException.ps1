b5b9927416a6d4a87931d79cb72466c92744ef0b
adam230594 <adam230594@users.noreply.github.com>
2022-09-14 17:51:50 +0100

    Addition of Remove-AppServiceIpException script and tests

---
+++ Infrastructure-Scripts/Remove-AppServiceIpException.ps1
@@ -0,0 +1,44 @@
+<#
+    .SYNOPSIS
+    Remove the access restriction rules from an app service
+
+    .DESCRIPTION
+    Remove the access restriction rules from an app service given an IP
+
+    .PARAMETER IpAddress
+    An ip address to associate with the access restriction rule
+
+    .PARAMETER ResourceName
+    The name of the app service
+
+    .EXAMPLE
+    Remove-AppServiceIpException -IpAddress 192.168.0.1 -ResourceName das-prd-sms-as
+#>
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNull()]
+    [IPAddress]$IpAddress,
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNull()]
+    [String]$ResourceName
+)
+
+
+$AppServiceResource = Get-AzResource -Name $ResourceName -ResourceType "Microsoft.Web/sites"
+
+if (!$AppServiceResource) {
+    throw "Could not find a resource matching $ResourceName in the subscription"
+}
+
+$AppServiceWhitelist = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $AppServiceResource.ResourceGroupName -Name $ResourceName | Where-Object { $_.MainSiteAccessRestrictions.IpAddress -eq "$IPAddress/32" }
+
+if (!$AppServiceWhitelist) {
+    Write-Output " -> Could not find whitelisted $IPAddress to remove!"
+}
+else {
+    Write-Output "  -> Removing $IPAddress"
+    Remove-AzWebAppAccessRestrictionRule  -ResourceGroupName $AppServiceResource.ResourceGroupName -WebAppName $ResourceName -IpAddress "$IPAddress/32"
+    Write-Output "  -> $IPAddress, removed!"
+}
---
+++ Tests/UT.Remove-AppServiceIpException.Tests.ps1
@@ -0,0 +1,65 @@
+$Config = Get-Content $PSScriptRoot\..\Tests\Configuration\Unit.Tests.Config.json -Raw | ConvertFrom-Json
+Set-Location $PSScriptRoot\..\Infrastructure-Scripts\
+
+Describe "Remove-AppServiceIpException Unit Tests" -Tags @("Unit") {
+
+    $Params = @{
+        IpAddress           = $Config.ipAddress
+        ResourceName        = $Config.resourceName
+    }
+
+    Context "Resource does not exist" {
+        It "The specified Resource was not found in the subscription, throw an error" {
+            Mock Get-AzResource -MockWith { return $null }
+            { ./Remove-AppServiceIpException @Params } | Should throw "Could not find a resource matching $($Config.resourceName) in the subscription"
+            Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
+        }
+    }
+
+    Context "Resources exists and app service ip exception doesn't exist" {
+        It "Should output existing access restrction with given IP does not exist" {
+
+            Mock Get-AzResource -MockWith {
+                return @{
+                    "ResourceGroupName" = $Config.resourceGroupName
+                    "Name"              = $Config.resourceName
+                }
+            }
+            Mock Get-AzWebAppAccessRestrictionConfig -MockWith { return $null }
+            Mock Write-Output -MockWith {}
+            { ./Remove-AppServiceIpException @Params } | Should Not throw
+            Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Get-AzWebAppAccessRestrictionConfig' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Write-Output' -Times 1 -Scope It -ParameterFilter { $InputObject -eq " -> Could not find whitelisted $($Config.ipAddress) to remove!" }
+        }
+    }
+
+    Context "Resources exists and app service ip exception exists" {
+        It "Should remove the ip access restriction to the found resources" {
+
+            Mock Get-AzResource -MockWith {
+                return @{
+                    "ResourceGroupName" = $Config.resourceGroupName
+                    "Name"              = $Config.resourceName
+                }
+            }
+            Mock Get-AzWebAppAccessRestrictionConfig -MockWith {
+                return @{
+                    "MainSiteAccessRestrictions" = @{
+                        "IpAddress" = "$($Config.ipAddress)/32"
+                    }
+                }
+            }
+            Mock Remove-AzWebAppAccessRestrictionRule -MockWith { return $null }
+            Mock Write-Output -MockWith {}
+            { ./Remove-AppServiceIpException @Params } | Should Not throw
+            Assert-MockCalled -CommandName 'Get-AzResource' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Get-AzWebAppAccessRestrictionConfig' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Remove-AzWebAppAccessRestrictionRule' -Times 1 -Scope It
+            Assert-MockCalled -CommandName 'Write-Output' -Times 2 -Scope It
+            Assert-MockCalled -CommandName 'Write-Output' -Times 1 -Scope It -ParameterFilter { $InputObject -eq "  -> Removing $($Config.ipAddress)" }
+            Assert-MockCalled -CommandName 'Write-Output' -Times 1 -Scope It -ParameterFilter { $InputObject -eq "  -> $($Config.ipAddress), removed!" }
+        }
+    }
+
+}
