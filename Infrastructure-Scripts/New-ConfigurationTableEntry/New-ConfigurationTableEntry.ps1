f4e2662ae29d8fef3b93a663a6ceadd52a18da2b
NickGraham101 <nick.graham101@googlemail.com>
2022-09-15 15:05:19 +0100

    Added docs to New-ConfigurationTableEntry

--- Infrastructure-Scripts/New-ConfigurationTableEntry/New-ConfigurationTableEntry.ps1
+++ Infrastructure-Scripts/New-ConfigurationTableEntry/New-ConfigurationTableEntry.ps1
@@ -1,3 +1,45 @@
+<#
+    .SYNOPSIS
+    Parses a JSON schema file into a configuration object and populates this with values from environment variables.
+
+    .DESCRIPTION
+    Parses a JSON schema file into a configuration object and populates this with values from environment variables.  Intended for use in Azure DevOps pipelines.
+    Will first try to get values from Azure DevOps variables before trying to get them from environment variables.  In this way Azure DevOps secret variables can be
+    accessed by mapping them to environment variables as part of a task definition.
+
+    After generation the configuration object is written to an Azure Storage Table.
+
+    .PARAMETER SourcePath
+    The path to the directory containing the schema file.
+
+    .PARAMETER TargetFilename
+    The filename of the schema.
+
+    .PARAMETER StorageAccountName
+    The name of the Azure Storage Account that contains the Table.
+
+    .PARAMETER StorageAccountResourceGroup
+    The name of the resource group containing the Storage Account.
+
+    .PARAMETER EnvironmentName
+    The environment the configuration is being created in, used as the Partition Key in the Storage Table.
+
+    .PARAMETER TableName
+    The table name, defaults to Configuration.
+
+    .PARAMETER Version
+    The configuration version, used as part of the row name.  Defaults to '1.0'.
+
+    .EXAMPLE
+    $NewConfigurationTableEntry = @{
+        SourcePath = "$(Pipeline.Workspace)/foo-bar-config/Configuration/foo-bar-web"
+        TargetFilename = "FOO.BAR.Web.schema.json"
+        StorageAccountName = "fooconfigstr"
+        StorageAccountResourceGroup = "foo-config-rg"
+        EnvironmentName   = "BAR"
+    }
+    .\New-ConfigurationTableEntry.ps1 @NewConfigurationTableEntry
+#>
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory = $True)]
