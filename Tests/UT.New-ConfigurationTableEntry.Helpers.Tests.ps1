ddbb13fd11678fb29d4ccb605965af2f401feb28
NickGraham101 <nick.graham101@googlemail.com>
2022-09-16 10:47:42 +0100

    Added additional contexts to New-ConfigurationEntity tests

--- Tests/UT.New-ConfigurationTableEntry.Helpers.Tests.ps1
+++ Tests/UT.New-ConfigurationTableEntry.Helpers.Tests.ps1
@@ -60,6 +60,34 @@ Describe "New-ConfigurationTableEntry New-ConfigurationEntity Helper Unit Tests"
         }
     }
 
+    Context "Storage table exists but row doesn't" {
+        Mock Get-AzTableRow -ModuleName Helpers
+
+        It "Should add a new row in the existing table" {
+            New-ConfigurationEntity @Params
+            Assert-MockCalled -CommandName New-AzStorageTable -ModuleName Helpers -Exactly -Times 0
+            Assert-MockCalled -CommandName Update-AzTableRow -ModuleName Helpers -Exactly -Times 0
+            Assert-MockCalled -CommandName Add-AzTableRow -ModuleName Helpers -Exactly -Times 1
+        }
+    }
+
+    Context "Storage table exists but row doesn't and there is an error trying to add the row" {
+        Mock Get-AzTableRow -ModuleName Helpers
+
+        Mock Add-AzTableRow -ModuleName Helpers -MockWith {
+            throw
+        }
+
+        It "Should throw an error and not write out a success message" {
+            { New-ConfigurationEntity @Params } | Should -Throw
+            Assert-MockCalled -CommandName New-AzStorageTable -ModuleName Helpers -Exactly -Times 0
+            Assert-MockCalled -CommandName Update-AzTableRow -ModuleName Helpers -Exactly -Times 0
+            Assert-MockCalled -CommandName Add-AzTableRow -ModuleName Helpers -Exactly -Times 1
+            Assert-MockCalled -CommandName Write-Host -ModuleName Helpers -Exactly -Times 1
+            Assert-MockCalled -CommandName Write-Host -ModuleName Helpers -ParameterFilter { $Object -match "^Configuration succesfully added to.*"} -Exactly -Times 0
+        }
+    }
+
     Context "Storage table and row already exist but there is an error trying to update the row" {
         Mock Update-AzTableRow -ModuleName Helpers -MockWith {
             throw
@@ -74,6 +102,7 @@ Describe "New-ConfigurationTableEntry New-ConfigurationEntity Helper Unit Tests"
             Assert-MockCalled -CommandName Write-Host -ModuleName Helpers -ParameterFilter { $Object -match "^Configuration succesfully added to.*"} -Exactly -Times 0
         }
     }
+}
 
 Describe "New-ConfigurationTableEntry Get-StorageAccountKey Helper Unit Tests" -Tags @("Unit") {
     Context "Passed a valid storage account name and resource group" {
