ca952bb3250c1ec95184b7c7bf5e34ba37787335
debrr07 <rdebrayeng@hotmail.com>
2023-04-18 18:09:42 +0100

    corrected mocked unit tests

--- Tests/UT.Add-WAFIPException.Tests.ps1
+++ Tests/UT.Add-WAFIPException.Tests.ps1
@@ -1,11 +1,33 @@
-Describe "Add IP address to WAF whitelist" {
-    Context " Check if Web Application Firewall exists" {
-        It 'should return a firewall policy' {
-            $Policy = @{
-                ResourceGroupName = 'myResourceGroup'
-                PolicyName = 'myPolicy'
+$Config = Get-Content $PSScriptRoot\..\Tests\Configuration\Unit.Tests.Config.json -Raw | ConvertFrom-Json
+Set-Location C:\Users\rdebr\OneDrive\Desktop\Repo\das-platform-automation\Infrastructure-Scripts
+
+Describe "Add-WAFIPException Unit Tests" -Tags @("Unit") {
+
+    $Params = @{
+        Name              = "TestUser"
+        IPAddress         = $Config.ipAddress
+        PolicyName        = $Config.resourceName
+        ResourceGroupName = $Config.resourceGroupName
+    }
+
+    Context "Web Application Firewall policy does not exist" {
+        It 'The specified Resource was not found in the resource group, throw an error' {
+            Mock Get-AzApplicationGatewayFirewallPolicy -MockWith { return $null }
+            { ./Add-WAFIPException -Name "TestUser" -IPAddress $Config.ipAddress -PolicyName "testpolicy" -ResourceGroupName $Config.resourceGroupName } | Should throw
+            Assert-MockCalled -CommandName 'Get-AzApplicationGatewayFirewallPolicy' -Times 1 -Scope It
+        }
+    }
+
+    Context "Web Application Firewall policy does exist" {
+        It 'The specified Resource was found in the resource group' {
+            Mock Get-AzApplicationGatewayFirewallPolicy -MockWith {
+                return @{
+                    "PolicyName"        = $Config.resourceName
+                    "ResourceGroupName" = $Config.resourceGroupName
+                }
+            { ./Add-WAFIPException @Params} | Should -Not throw
+            Assert-MockCalled -CommandName 'Get-AzApplicationGatewayFirewallPolicy' -Times 1 -Scope It
             }
-            Mock Get-AzApplicationGatewayFirewallPolicy -MockWith { return $Policy } | Should Not Be NullOrEmpty
         }
     }
 
@@ -13,23 +35,23 @@ Describe "Add IP address to WAF whitelist" {
         $WafPolicy = @{
             CustomRules = @{
                 MatchCondition = @{
-                    MatchValues = @('192.168.0.10', '10.0.0.1')
+                    MatchValues = @("192.168.0.10", "10.0.0.1")
                 }
             }
         }
         It "does exist" {
-            $IPAddress = '10.0.0.1'
+            $IPAddress = "10.0.0.1"
 
             $IPExists = $WafPolicy.CustomRules | Where-Object { $_.MatchCondition.MatchValues -contains $IPAddress }
 
             $IPExists | Should Be $true
         }
         It "does not exist" {
-            $IPAddress = $null
+            $IPAddress = ""
 
             $IPExists = $WafPolicy.CustomRules | Where-Object { $_.MatchCondition.MatchValues -contains $IPAddress }
 
-            $IPExists | Should Not Be $true
+            $IPExists | Should -Not -Be $true
         }
     }
 
@@ -60,7 +82,7 @@ Describe "Add IP address to WAF whitelist" {
         }
     }
 
-    Context "Creation of custom rule" {
+    Context "New custom rule for the web application firewall policy created" {
         $NewCustomRule = @{
             Name = 'test'
             Priority = '3'
@@ -69,27 +91,37 @@ Describe "Add IP address to WAF whitelist" {
             Action = 'Allow'
         }
         It 'should create a custom rule' {
-            Mock New-AzApplicationGatewayFirewallCustomRule -MockWith { return $NewCustomRule } | Should Not Be NullOrEmpty
+            Mock New-AzApplicationGatewayFirewallCustomRule -MockWith { 
+                return @{
+                    Name = 'test'
+                    Priority = '3'
+                    RuleType = 'MatchRule'
+                    MatchCondition = 'MatchCondition'
+                    Action = 'Allow'
+                }
+            { ./Add-WAFIPException @Params} | Should -Not throw
+            Assert-MockCalled -CommandName 'New-AzApplicationGatewayFirewallCustomRule' -Times 1 -Scope It
+            }
         }
         It 'should add a new custom rule' {
             $WafPolicy = @{
                 CustomRules = @()
             }
-            $IPExists = $null
+            $IPExists = $true
             if ($IPExists) {
                 $CustomRuleCreated = $WafPolicy.CustomRules += $NewCustomRule
             }
-            $CustomRuleCreated | Should Not Be NullOrEmpty
+            $CustomRuleCreated | Should -Not -BeNullOrEmpty
         }
         It 'should not add a new custom rule' {
             $WafPolicy = @{
                 CustomRules = @()
             }
-            $IPExists = $null
+            $IPExists = $true
             if (!$IPExists) {
                 $CustomRuleCreated = $WafPolicy.CustomRules
             }
-            $CustomRuleCreated | Should Be $null
+            $CustomRuleCreated | Should -BeNullOrEmpty
         }
     }
 }
